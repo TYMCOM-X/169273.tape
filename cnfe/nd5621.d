492b43d04f2187dd2437c4c35af136ad99aa146e
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:32:18 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd5621.d
@@ -0,0 +1,57 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 5621   DISTRICT: DALLAS    DATE BLACKBOOK UPDATED: NOV 07,1986
+LAST PROJECT NUMBER: 18427CDA001 DATE COMPLETED : NOV 02,1986
+FIELD ENGINEER CONTACT : BILL SCHNELLER
+******************************* L O C A T I O N *******************************
+CUSTOMER NAME: TYMNET INC.            ADDRESS: 6333 HARRY HINES BLVD.
+CITY/STATE/ZIP: DALLAS,TEXAS 75235
+CUSTOMER PHONE: 214/637-7316          TYMNET FE PHONE:
+******************************* C O N T A C T S *******************************
+8-5PM M-F: CNFED PERSONNEL          WORK:
+AFTER 5: STANDBY PERSON 
+********************************* A C C E S S *********************************
+COMMENTS:
+******************************* H A R D W A R E *******************************
+SERIAL NUMBER: 1817             ASSET TAG NUMBER 221239
+MACHINE TYPE: ENGINE            ;MACHINE CONFIG. TYPE: T-II ISIS
+KERNEL HOST # = 7165            ;M/F BOARD = LBOOT6
+FILE NAME = (TYM5)ND5621.BND    ;FIRMWARE = VERSION 18
+MAC TYPE = MAC II               ;MEMORY TYPE & AMOUNT = 1 MEG ZITEL
+NO. OF SYNC CARDS = 1           ;NO. OF ASYNC CARDS =
+NO. OF SIO MBS =                ;NO. OF SIO DBS =
+TOTAL PARADYNES =               ;MPX =          ;MP96 =
+NO. OF HOST SLOTS = 4           ;NO. OF DATATROLS =
+NO. OF VADICS =                 ;NO. OF 202T'S =
+NO. OF CONNECTOR PANELS = 1     ;NO. OF MODEM BREAKOUT PANELS = 1
+FCO'S COMPLETE?                 ;NO. OF ASYNC BREAKOUT PANELS =
+NO. OF PRINTERS =
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO: 214/823-4171       LOCAL: 214/824-1121
+************** N E T W O R K   N O D E   &   H O S T   L I N K S **************
+LINE 00 = 9.6      ND5330  KH#6730  (L-09) HARDWIRE
+LINE 01 = 9.6      ND5612  KH#7139  (L-09) HARDWIRE
+LINE 02 = 9.6      ND5620  KH#7163  (L-02) HARDWIRE
+************** C U S T O M E R    I N T E R F A C E   S L O T S ***************
+SLOT 1   HOST#= 1583;CUSTOMER= MDC-PSC
+ADDRESS:                     ;CITY/STATE/ZIP:
+PHONE:               ; CONTACT=                ; HOST TYPE:PROTOCOL CONVERTER
+L.U. = 1   ;A.REF =       ;S.REF = 2    ;M.REF =
+SLOT 2   HOST#= 3767;CUSTOMER= ELECTRONIC DATA SYSTEMS
+ADDRESS:                     ;CITY/STATE/ZIP:
+PHONE:               ; CONTACT=                ; HOST TYPE: SHI-3270/B
+L.U. = 1   ;A.REF =       ;S.REF = 3    ;M.REF =
+SLOT 3   UNUSED
+ADDRESS:                     :CITY/STATE/ZIP:
+PHONE:               ; CONTACT=                ; HOST TYPE: CMT
+L.U. = 0   ;A.REF =       ;S.REF = 0    ;M.REF =
+SLOT 4   HOST#=4038 ;CUSTOMER= EXXON
+PHONE:                CONTACT=           HOST TYPE:GATEWAY
+LU = 1     ;A.REF=         ;S.REF =4     
+SLOT 5   HOST#=8331 ;CUSTOMER= ERICSSON NETWORK SYSTEMS
+PHONE:                CONTACT=           HOST TYPE: MENU SERVER
+LU = (CODE ONLY)
+
+************************** PARADYNE SERIAL NUMBERS ****************************
+CKT # 11FDPA028797     ;S/N 149181  ;A/N 251244             ;CAB 3,78
+CKT # FDEA731517       ;S/N 6267R   ;A/N 249241             ;CAB 4,7
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
