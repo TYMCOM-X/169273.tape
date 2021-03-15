c55d669634e3703cf7507548ae18ee0dfb09dd4d
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:38:28 +0000

    Files extracted from 169273.tape

---
+++ cnfe/sp2265.d
@@ -0,0 +1,46 @@
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 2265                 DISTRICT: DALLAS  86/03/07  14:22
+LAST PROJECT NUMBER : -
+FIELD ENGINEER CONTACT : COLEMAN RHOADES - RON BARDIN
+
+********************************************************************
+                                 **LOCATION**
+CUSTOMER NAME : SWB  ADDRESS : 4100 BRYAN ST
+CITY/STATE/ZIP :                            DALLAS
+CUSTOMER PHONE : 214-464-5243 TYMNET FE PHONE : -
+
+********************************************************************
+                                 **CONTACTS**
+8-5PM : SETH WOOD    WORK : 464-5243
+
+AFTER 5 : -    TEL : -
+
+********************************************************************
+                                 **ACCESS COMMENTS**
+ACESS THROUGH GUARD AT FRONT DOOR
+CALL BEFORE DISPATCHING
+-
+**
+
+********************************************************************
+PVC OR PAP CODE REQUIRED? NO  HIGH SPEED ASYNC? NO
+********************************************************************
+                                 **HARDWARE**
+SERIAL NUMBER: 0628
+MACHINE TYPE : MICRO  MACHINE CONFIG. TYPE: -
+KERNEL HOST # = -  FILENAME= -
+ASSET TAG NUMBER = -
+
+MICRO-ENGINE PART NUMBER IS 57001-001 REV LEVEL -
+CONNECTOR PANEL  1 IS PART # AND REV LEVEL : -
+THIS CONNECTOR PANEL IS S.N. = 2568-0004
+CONNECTOR PANEL  2 IS PART # AND REV LEVEL : -
+THIS CONNECTOR PANEL IS S.N. = 2517-0016
+
+********************************************************************
+                                 **TELCO TROUBLE**
+TELCO TROUBLE NUMBER :-
+
+********************************************************************
+                                 **NETWORK LINKS**
+********************************************************************
