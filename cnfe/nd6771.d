27e6064ce46001ca55e7e30817af10d96adec535
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:33:13 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd6771.d
@@ -0,0 +1,54 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 6771   DISTRICT: DALLAS    DATE BLACKBOOK UPDATED: AUGUST 19, 1985
+LAST PROJECT NUMBER:12824        DATE COMPLETED : AUGUST 15, 1985
+FIELD ENGINEER CONTACT : RON BARDIN  CNFED
+
+******************************* L O C A T I O N *******************************
+CUSTOMER NAME:TYMNET/FT. SMITH ANSWER ADDRESS: 2601 SOUTH "V" ST. 1ST. FL.
+CITY/STATE/ZIP:FT. SMITH, ARKANSAS 72901                
+CUSTOMER PHONE: 501/782-1100          TYMNET FE PHONE:501/782-8654
+
+******************************* C O N T A C T S *******************************
+8-5PM M-F: HUGH JONES               WORK:501/782-1100       HOME:
+
+*AFTER 5: ANYONE WHO IS THERE.      TEL: SAME AS ABOVE.
+
+********************************* A C C E S S *********************************
+COMMENTS:
+       THIS IS AN ANSWERING SERVICE AND YOU CAN ASK WHOEVER ANSWERS
+THE PHONE, (AFTER 5PM.), TO HELP YOU OR GET IN TOUCH WITH MR. JONES.
+              THIS SITE IS MANNED 24 HOURS A DAY
+*******************************************************************************
+PVC OR PAP CODE REQUIRED? NO     HIGH SPEED ASYNC? NO
+*******************************************************************************
+
+******************************* H A R D W A R E *******************************
+SERIAL NUMBER: M968
+MACHINE TYPE: MICRO             ;MACHINE CONFIG. TYPE: TYMSAT
+KERNEL HOST # = 1297            ;M/F BOARD =
+FILE NAME =(TYM4000)ND6771.BND  ;FIRMWARE =
+MAC TYPE =                      ;MEMORY TYPE & AMOUNT =ZITEL 1/2 MEG
+NO. OF SYNC CARDS =             ;NO. OF ASYNC CARDS =
+NO. OF SIO MBS =                ;NO. OF SIO DBS =
+TOTAL PARADYNES = 1             ;MPX = 1        ;MP96 =
+NO. OF HOST SLOTS =             ;NO. OF DATATROLS =
+NO. OF VADICS = 8               ;NO. OG 202T'S =
+NO. OF CONNECTOR PANELS =       ;NO. OF MODEM BREAKOUT PANELS =
+FCO'S COMPLETE?                 ;NO. OF ASYNC BREAKOUT PANELS =
+NO. OF PRINTERS =
+
+
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO:501-                LOCAL: 214-824-1121
+
+********************** N E T W O R K   L I N K S ******************************
+LINE 00 =   
+LINE 01 =
+LINE 02 =
+LINE 03 = 9.6    TO  ND 5326 (LN-00) KH. #6728
+
+************************** PARADYNE SERIAL NUMBERS ****************************
+CKT # FDEA326291       ;S/N 199869
+CKT #                  ;S/N         :CKT #                  ;S/N
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
