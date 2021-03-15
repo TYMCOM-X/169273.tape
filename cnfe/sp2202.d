1ee84fadc56dadf238bb8d2cdeaff37624fa048e
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:35:50 +0000

    Files extracted from 169273.tape

---
+++ cnfe/sp2202.d
@@ -0,0 +1,53 @@
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 2202                 DISTRICT: DALLAS  86/02/27  07:12
+LAST PROJECT NUMBER : 
+FIELD ENGINEER CONTACT : ED DOBBS
+
+********************************************************************
+                                 **LOCATION**
+CUSTOMER NAME : SWB  ADDRESS : 4100 BRYAN ST
+CITY/STATE/ZIP :                            DALLAS
+CUSTOMER PHONE : 214-464-5482 TYMNET FE PHONE : 214-637-7440
+
+********************************************************************
+                                 **CONTACTS**
+8-5PM : SETH WOOD    WORK : -
+
+AFTER 5 : -    TEL : -
+
+********************************************************************
+                                 **ACCESS COMMENTS**
+-
+**
+
+
+
+********************************************************************
+PVC OR PAP CODE REQUIRED? NO  HIGH SPEED ASYNC? YES
+********************************************************************
+                                 **HARDWARE**
+SERIAL NUMBER: 2569
+MACHINE TYPE : FULL BLOWER  MACHINE CONFIG. TYPE: -
+KERNEL HOST # = 714   FILENAME= -
+ASSET TAG NUMBER = NA
+REV LEVEL OF M/F = 84-044 (430118-001) S.N.= 2144-0006
+REV LEVEL OF ROM I/O = - (430136-001) S.N.= 2138-0097
+REV LEVEL OF CPU = - (430137-001) S.N.= 21372137-030
+REV LEVEL OF MAC = - (430109-001) S.N.= 0227-0177
+MEMORY PART NUMBER AND REV LEVEL = REV C S.N.= V1384-0148
+SYNC BOARD  1 IS REV LEVEL - (430111-001) S.N.= 2135-013
+ASYNC BOARD  1 IS REV LEVEL 84-061 (430110-001) S.N.= NA
+SIO MOTHER BOARD  1 IS REV LEVEL H (430123-001) S.N.= 2533-0052
+SIO DAUGHTER BD.  1 IS A V24 (430128-001) S.N.= 1379-1208
+SIO DAUGHTER BD.  2 IS A ASYNC (430125-001) S.N.= V933-0087
+SIO DAUGHTER BD.  3 IS A ASYNC (430125-001) S.N.= 1725-2000
+SIO DAUGHTER BD.  4 IS A ASYNC (430125-001) S.N.= NA
+SIO DAUGHTER BD.  5 IS A ASYNC (430125-001) S.N.= NA
+
+********************************************************************
+                                 **TELCO TROUBLE**
+TELCO TROUBLE NUMBER :-
+
+********************************************************************
+                                 **NETWORK LINKS**
+********************************************************************
