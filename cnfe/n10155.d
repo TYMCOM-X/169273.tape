a00f2cf1744020c9c5b2e47f495cdc27ef6a7c9f
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:30:49 +0000

    Files extracted from 169273.tape

---
+++ cnfe/n10155.d
@@ -0,0 +1,79 @@
+
+NODE:N10155  DISTRICT: DALLAS    DATE BLACKBOOK UPDATED:8/15/86
+LAST PROJECT NUMBER:18379        DATE COMPLETED :8/15/86
+FIELD ENGINEER CONTACT :DOUG MILLER
+
+******************************* L O C A T I O N *******************************
+CUSTOMER NAME:DRESSER                 ADDRESS:17201 WATERVIEW PKY
+CITY/STATE/ZIP:DALLAS/TX/75379
+CUSTOMER PHONE:680-4214               TYMNET FE PHONE:214/637-7440
+
+******************************* C O N T A C T S *******************************
+8-5PM M-F:AL KOZAR                  WORK:680-4214
+                                    WORK:                   HOME:
+*AFTER 5:SECURITY GUARD             TEL:680-4214
+
+********************************* A C C E S S *********************************
+COMMENTS:SECURITY GUARD WILL CONNECT YOU AFTER HOURS TO COMPROOM.  SO
+YOU CAN GAIN ACCESS THROUGH PEOPLE IN COMPROOM.
+
+
+*******************************************************************************
+PVC OR PAP CODE REQUIRED?NO      HIGH SPEED ASYNC?NO
+*******************************************************************************
+
+ASSET TAG #0004411
+******************************* H A R D W A R E *******************************
+SERIAL NUMBER:M1543
+MACHINE TYPE:MICROO             ;MACHINE CONFIG. TYPE:ISIS
+KERNEL HOST # =6602             ;M/F BOARD =
+FILE NAME =(TYM5)N10155.BND
+MAC TYPE =                      ;MEMORY TYPE & AMOUNT =1 MEG
+NO. OF SYNC CARDS =             ;NO. OF ASYNC CARDS =
+NO. OF SIO MBS =                ;NO. OF SIO DBS =
+TOTAL PARADYNES =2              ;MPX =2
+NO. OF HOST SLOTS =2
+NO. OF VADICS =                 ;NO. OF 202T'S =
+NO. OF CONNECTOR PANELS =       ;NO. OF MODEM BREAKOUT PANELS =
+FCO'S COMPLETE?                 ;NO. OF ASYNC BREAKOUT PANELS =
+NO. OF PRINTERS =
+
+************************ PAP AND/OR PVC INFORMATION ***************************
+
+
+
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO:824-1121            LOCAL:824-1121
+
+************** N E T W O R K   N O D E   &   H O S T   L I N K S **************
+LINE 10 =2754,2426,2,
+LINE 11=2754,2426,2
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+************** C U S T O M E R    I N T E R F A C E   S L O T S ***************
+SLOT 1   HOST#=6608 ;CUSTOMER=DRESSER
+ADDRESS:17201 WATERVIEW PKY  ;CITY/STATE/ZIP:DALLAS TX 75379
+PHONE:680-4214       ; CONTACT=AL KOZAR        ; HOST TYPE:3270
+L.U. =1    ;A.REF =       ;S.REF =2     ;M.REF =
+SLOT 2   HOST#=6609 ;CUSTOMER=DRESSER
+ADDRESS:SAME                 ;CITY/STATE/ZIP:SAME
+PHONE:SAME           ; CONTACT=                ; HOST TYPE:CMT
+L.U. =0    ;A.REF =       ;S.REF =      ;M.REF =
+
+************************** PARADYNE SERIAL NUMBERS ****************************
+CKT #FDDA313214        ;S/N3324     :CKT #SAME              ;S/N4581
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
