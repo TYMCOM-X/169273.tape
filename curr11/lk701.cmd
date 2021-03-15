6ae073707b7735ae2ba0ea08760e05e977bf4469
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 08:10:12 +0000

    Files extracted from 169273.tape

---
+++ curr11/lk701.cmd
@@ -0,0 +1,9 @@
+sv701/img/b:1000,mp701=cg701,ru701,xdt,
+exec,error,pkt1,pkt2,pkt3d,
+cmdtym,cmds,trm,trmf,trmptp,
+trmt,zapbuf,
+lnk1,lnk2,lnkrsp,lnkd,
+lnkha,lkhtp1,lkhtp2,lkhtp3,lkhtpc,
+tyma,tymb,
+lina,status,once,end
+
