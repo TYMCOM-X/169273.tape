b0ca470e1c604b0792e0d3834e4756cf00e0a7bb
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 08:12:31 +0000

    Files extracted from 169273.tape

---
+++ curr11/lk207.cmd
@@ -0,0 +1,8 @@
+SV207.IMG/img/b:1000,MP207=CG207,RU207,
+execmm,error,pkt1,pkt2,pkt3d,
+cmdtym,cmds,trm,trmf,trmp,
+trmt,zapbuf,status,
+lnk1,lnk2,lnkf,LNKF1,
+LNKHA,LKHBS1,LKHBS2,LKHBS3,
+TYMA,TYMB,
+lina,ONCE,END
