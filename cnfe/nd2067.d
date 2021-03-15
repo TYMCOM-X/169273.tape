c2de80bdd0e9b85773da8cb691a09fe890fc301f
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:34:18 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd2067.d
@@ -0,0 +1,61 @@
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 2067                 DISTRICT: DALLAS  11-12-86
+LAST PROJECT NUMBER : 16919
+FIELD ENGINEER CONTACT : EDWIN D. IVY
+
+********************************************************************
+                                 **LOCATION**
+CUSTOMER NAME : AMERICAN AIRLINES   ADDRESS : 4200 AMERICAN BLVD.
+CITY/STATE/ZIP :                            FORT WORTH/TX.
+CUSTOMER PHONE : 817/355-1164 TYMNET FE PHONE : 817/355-1659
+
+********************************************************************
+                                 **CONTACTS**
+8-5PM : JERRY KLUTZ        WORK : 214/355-1162
+
+AFTER 5 :      TEL : NA
+
+********************************************************************
+                                 **ACCESS COMMENTS**
+AFTER HOURS ACCESS CAN BE OBTAINED BY CONTACTING SECURITY GUARD ON
+PREMISES.  HIS/HER NUMBER IS 355-1621. ENTRY IS SUBJECT TO PRIOR
+APPROVAL BY DAYTIME CONTACT(S).
+**
+
+********************************************************************
+PVC OR PAP CODE REQUIRED? NO  HIGH SPEED ASYNC? YES
+********************************************************************
+                                 **HARDWARE**
+SERIAL NUMBER: NA
+MACHINE TYPE : MINI   MACHINE CONFIG. TYPE: TYMSAT
+KERNEL HOST # = 3122  FILENAME= (TYMNET)ND2067.BND
+ASSET TAG NUMBER = NA
+REV LEVEL OF M/F = G (430118-001) S.N.= NA
+REV LEVEL OF ROM I/O = J (430136-001) S.N.= NA
+REV LEVEL OF CPU = K (430137-001) S.N.= NA
+REV LEVEL OF MAC = H (430109-001) S.N.= NA
+MEMORY PART NUMBER AND REV LEVEL = C S.N.= NA
+SYNC BOARD  1 IS REV LEVEL D (430111-001) S.N.= NA
+ASYNC BOARD  1 IS REV LEVEL D (430110-001) S.N.= NA
+THERE ARE 2 MP9600 MODEMS (600028-001)
+CONNECTOR PANEL  1 IS PART # AND REV LEVEL : NA
+THIS CONNECTOR PANEL IS S.N. = NA
+
+********************************************************************
+                                 **TELCO TROUBLE**
+TELCO TROUBLE NUMBER : 817/998-4109=LOCAL  214/823-4171=TOLL
+
+********************************************************************
+                                 **NETWORK LINKS**
+LINE     0 = FDEA307695/PDY TO N-2741 KH2388 L3
+LINE     3 = FDEA307686/PDY 4.8 TO N-2401 KH1840 L6
+********************************************************************
+
+PLEASE BE ADVISED THAT THE AMERICAN AIRLINES LOCAL ROTARY HAS BEEN
+CONVERTED FROM VADIC 3 PHASE MODEMS TO CONCORD RACKMOUNTED 224 (2400
+BAUD) MODEMS WITH 1200 BAUD FALLBACK CAPABILITY. THE PILOT FOR THIS
+ROTARY IS 214/355-1660 WHICH SERVICES 27 LINES.
+THERE IS ALSO A VADIC CHASIS REMAINING WITH 5 VADIC MODEMS FOR LOW
+SPEED USERS AT THE END OF THE 2400 BAUD ROTARY. THE PILOT IS 355-
+1689.
+
