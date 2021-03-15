1bf1ed8d0cfbf690536cc1ba881f92962337f84d
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:36:20 +0000

    Files extracted from 169273.tape

---
+++ cnfe/uc2026.d
@@ -0,0 +1,74 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 2026   DISTRICT: DALLAS    DATE BLACKBOOK UPDATED: APRIL 20, 1984
+LAST PROJECT NUMBER:             DATE COMPLETED :
+FIELD ENGINEER CONTACT : CNFED
+
+******************************* L O C A T I O N *******************************
+CUSTOMER NAME:TEXAS UTILITIES GEN.CO. ADDRESS:COMANCHE PEAK/FM RD 201
+CITY/STATE/ZIP:GLEN ROSE,TEXAS 
+CUSTOMER PHONE: 817/897-4881          TYMNET FE PHONE:
+
+******************************* C O N T A C T S *******************************
+8-5PM M-F:FRED MADDEN               WORK:817/897-4881 X601  HOME:
+                                    WORK:                   HOME:
+*AFTER 5:                           TEL:
+
+********************************* A C C E S S *********************************
+COMMENTS:ACCESS INFO GIVEN AT TIME OF MAINTENANCE CALL VIA UCC DALLAS.
+
+
+
+*******************************************************************************
+PVC OR PAP CODE REQUIRED? N      HIGH SPEED ASYNC? N
+*******************************************************************************
+
+******************************* H A R D W A R E *******************************
+MACHINE TYPE: T-II MINI-ENGINE  ;MACHINE CONFIG. TYPE: TYMSAT
+KERNEL HOST # = 32              ;M/F BOARD = 3 POS. SWT./LBOOT6 PROMS
+FILE NAME = (UCCPAC)ND2026.BND  ;FIRMWARE = VERSION 18 PROMS/ROM & I/O
+MAC TYPE = MAC I                ;MEMORY TYPE & AMOUNT =AMPEX/128K
+NO. OF SYNC CARDS = 1           ;NO. OF ASYNC CARDS = 1
+NO. OF SIO MBS =                ;NO. OF SIO DBS =
+TOTAL PARADYNES =               ;MPX =          ;MP96 =
+NO. OF HOST SLOTS =             ;NO. OF DATATROLS =
+NO. OF VADICS =                 ;NO. OG 202T'S =
+NO. OF CONNECTOR PANELS = 1     ;NO. OF MODEM BREAKOUT PANELS =
+FCO'S COMPLETE? Y               ;NO. OF ASYNC BREAKOUT PANELS =
+NO. OF PRINTERS =
+
+************************ PAP AND/OR PVC INFORMATION ***************************
+
+
+
+
+
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO:214/823-4171        LOCAL:214/824-1121
+
+********************** N E T W O R K   L I N K S ******************************
+LINE 00 = (4.8)16FDPA008839 TO UC2022 (L-05)K.H.=
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+
+************************** PARADYNE SERIAL NUMBERS ****************************
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
