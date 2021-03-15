108b2a3724ffb59352c9a659c351b12f64eb13f5
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:37:39 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd4445.d
@@ -0,0 +1,43 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 4445   DISTRICT: DALLAS    DATE BLACKBOOK UPDATED: MARCH 5, 1985
+LAST PROJECT NUMBER: 11384       DATE COMPLETED : MARCH 5,1985
+FIELD ENGINEER CONTACT : MARK ROGACZEWSKI
+
+******************************* L O C A T I O N *******************************
+CUSTOMER NAME: TYMSHARE               ADDRESS: DYNATAX PRINT CENTER/TYMSHARE
+CITY/STATE/ZIP: DALLAS/TX/75235                6333 HARRY HINES
+CUSTOMER PHONE:214//638-5700          TYMNET FE PHONE: 214//637-7416
+
+******************************* C O N T A C T S *******************************
+8-5PM M-F: PAT LYNCH                WORK: 214/638-5700      HOME: 214/350-6417
+           GARY CHANLEE                       638-5700                363-8760
+*AFTER 5:                           TEL:
+
+********************************* A C C E S S *********************************
+COMMENTS:
+
+
+
+*******************************************************************************
+PVC OR PAP CODE REQUIRED? NO     HIGH SPEED ASYNC? NO
+*******************************************************************************
+
+******************************* H A R D W A R E *******************************
+MACHINE TYPE: MINI-ISIS-II      ;MACHINE CONFIG. TYPE:TYMSAT
+KERNEL HOST # =3178             ;M/F BOARD =3 POS.SWCH/VERSION 6
+FILE NAME =(TYMNET)ND4445.BND   ;FIRMWARE =VERSION 18
+MAC TYPE = 2                    ;MEMORY TYPE & AMOUNT =512K ZITEL
+NO. OF SYNC CARDS = 1           ;NO. OF ASYNC CARDS = 1
+NO. OF SIO MBS = 0              ;NO. OF SIO DBS = 0
+TOTAL PARADYNES = 0             ;MPX = 0        ;MP96 = 0
+NO. OF HOST SLOTS = 0           ;NO. OF DATATROLS = 0
+NO. OF VADICS = 0               ;NO. OG 202T'S = 0
+NO. OF CONNECTOR PANELS = 1     ;NO. OF MODEM BREAKOUT PANELS = 0
+FCO'S COMPLETE? Y               ;NO. OF ASYNC BREAKOUT PANELS = 2
+NO. OF PRINTERS = 0
+********************** N E T W O R K   L I N K S ******************************
+LINE 00 =OPEN
+LINE 01 =OPEN
+LINE 02 = (9.6) HW. ND5331, KH.6731, LN.0
+LINE 03 = (9.6) HW. ND5327, KH.6729, LN.0
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
