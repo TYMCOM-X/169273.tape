718fff88e1131e77b2a84dde9cf8547cb1a0dfde
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:37:22 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd4060.d
@@ -0,0 +1,57 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 4060   DISTRICT: DALLAS    DATE BLACKBOOK UPDATED:8/12/85
+LAST PROJECT NUMBER:             DATE COMPLETED :
+FIELD ENGINEER CONTACT :RON BARDIN     CNFED
+
+******************************* L O C A T I O N *******************************
+CUSTOMER NAME: TYMNET                 ADDRESS: 6333 HARRYHINES BLVD.
+CITY/STATE/ZIP: DALLAS,TEXAS   75235
+CUSTOMER PHONE:214/637-7440           TYMNET FE PHONE:SAME
+
+******************************* C O N T A C T S *******************************
+8-5PM M-F: ALEX MARTEL/C.RHOADES    WORK:214/637-7440       HOME: N/A
+*AFTER 5: STAND-BY FE               TEL: 214-781-8081 (BEEPER)
+
+********************************* A C C E S S *********************************
+            THIS IS A  TYMNET/MARS PLUS ENGINE
+*******************************************************************************
+PVC OR PAP CODE REQUIRED?NO      HIGH SPEED ASYNC?NO
+*******************************************************************************
+******************************* H A R D W A R E *******************************
+MACHINE TYPE: TYMNET II(ENGINE) ;MACHINE CONFIG. TYPE: ISIS
+KERNEL HOST # =3896             ;M/F BOARD = 3 POS. SW
+FILE NAME =(TYMNET)ND4060.BND   ;FIRMWARE = SYNC ENHANCED
+MAC TYPE = II                   ;MEMORY TYPE & AMOUNT = ZITEL 1 MEG.
+NO. OF SYNC CARDS =1            ;NO. OF ASYNC CARDS =0
+NO. OF SIO MBS =0               ;NO. OF SIO DBS =0
+TOTAL PARADYNES =0              ;MPX =0         ;MP96 =0
+NO. OF HOST SLOTS =2            ;NO. OF DATATROLS =0
+NO. OF VADICS =0                ;NO. OF 202T'S =0
+NO. OF CONNECTOR PANELS =1      ;NO. OF MODEM BREAKOUT PANELS =0
+FCO'S COMPLETE?YES              ;NO. OF ASYNC BREAKOUT PANELS =0
+NO. OF PRINTERS =0
+
+************************ PAP AND/OR PVC INFORMATION ***************************
+                               N O N E
+
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO:214/824-1121        LOCAL: 214/823-4171
+
+************** N E T W O R K   N O D E   &   H O S T   L I N K S **************
+LINE 00 = (9.6) H/W  4713  KH.5148  L-5
+LINE 01 = (9.6) H/W  4067  KH.3960  L-9
+LINE XX =
+LINE XX =
+************** C U S T O M E R    I N T E R F A C E   S L O T S ***************
+SLOT 1   HOST#=2245 ;CUSTOMER= TYMNET TRAVEL/MARS-PLUS
+ADDRESS: SAME AS ABOVE       ;CITY/STATE/ZIP: SAME AS ABOVE
+PHONE:214/637-7442   ; CONTACT= ALEX MARTEL    ; HOST TYPE:SYNC/INT-FACE
+L.U. =4    ;S.REF (2,3,6,7)
+SLOT 2   HOST#=3661 ;CUSTOMER= TYMNET TRAVEL/MARS-PLUS
+ADDRESS:SAME                 ;CITY/STATE/ZIP:SAME
+PHONE:214-637-7442   ; CONTACT= ALEX MARTEL    ;HOST TYPE:SYNC-POLLED IN.
+L.U. =4    ;S.REF = (10,11,12,14)
+
+*******************************************************************************
+                     T H E                E N D
+*******************************************************************************
