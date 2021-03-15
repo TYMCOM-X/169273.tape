45ec570f93dd4e4d4889581f94ada81616b21107
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:33:58 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd2706.d
@@ -0,0 +1,60 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 2706   DISTRICT: DALLAS    DATE BLACKBOOK UPDATED: 11-13-86
+LAST PROJECT NUMBER: 13441       DATE COMPLETED : 27 JUNE, 1985
+FIELD ENGINEER CONTACT : RON BARDIN
+
+******************************* L O C A T I O N *******************************
+CUSTOMER NAME: TYMSHARE INC.          ADDRESS: 2101 W. WALL AVE. SUITE 5
+CITY/STATE/ZIP:                                MIDLAND,TEXAS 79701
+CUSTOMER PHONE: 915/682-3481          TYMNET FE PHONE: 915/682-3481
+
+******************************* C O N T A C T S *******************************
+8-5PM M-F: SANDY                    WORK: 915/683-8112     HOME:    
+           ROBERT MCCLENDON         WORK: 915/683-8112     HOME:
+*AFTER 5 & WEEKENDS: ROBERT MCCLENDON WILL BE IN OFFICE ON WEEKENDS, AFTER
+5PM, WHOEVER IS IN THE OFFICE; UNTIL FURTHER UPDATE.
+********************************* A C C E S S *********************************
+COMMENTS: KEYS FOR THE SITE (FRONT ENTRANCE AND SUITE 5 ) ARE LOCATED IN
+          IN DALLAS OFFICE FOR ACCESS 24HRS., 7 DAYS A WEEK.  ONLY THE
+          ABOVE CONTACTS SHOULD BE MADE.***** NO ONE ELSE!! ****
+*******************************************************************************
+PVC OR PAP CODE REQUIRED? NO     HIGH SPEED ASYNC? NO
+*******************************************************************************
+
+******************************* H A R D W A R E *******************************
+MACHINE TYPE: FULL BLOWN ENGINE ;MACHINE CONFIG. TYPE: T-II TYMSAT
+KERNEL HOST # = 2326            ;M/F BOARD = LBOOT6 3 POSITION SWITCH
+FILE NAME = (TYMNET)ND2706.BND  ;FIRMWARE = VERSION 17 PROMS
+MAC TYPE = MAC II               ;MEMORY TYPE & AMOUNT = 512K SEMI
+NO. OF SYNC CARDS = 1           ;NO. OF ASYNC CARDS = 4
+NO. OF SIO MBS = 0              ;NO. OF SIO DBS = 0
+TOTAL PARADYNES = 2             ;MPX = 0        ;MP96 = 2
+NO. OF HOST SLOTS = 0           ;NO. OF DATATROLS = 0
+NO. OF VADICS = 39              ;NO. OG 202T'S = 0
+NO. OF CONNECTOR PANELS = 1     ;NO. OF MODEM BREAKOUT PANELS = 0
+FCO'S COMPLETE? YES             ;NO. OF ASYNC BREAKOUT PANELS = 0
+NO. OF PRINTERS = 0
+
+************************ PAP AND/OR PVC INFORMATION ***************************
+AMF TUBOSCOPE  OCTAL PORT-74  915/682-0778
+************************ROTOR INFORMATION *************************************
+LOCAL ROTOR - 24 VADIC LINES - PILOT 915/683-5645
+FX ROTOR- TO AMARILLO - 806/383-0304, 0305, 0306,& 806/381-2831
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO: 915/684-4966       LOCAL: 915/684-7431
+
+********************** N E T W O R K   L I N K S ******************************
+LINE 00 = (9.6)FDE?-----/PDY TO ND2042 K.H.1682 LN.07
+LINE 01 = (4.8)FDEC92473/PDY TO N-2710 K.H. 2327 L.1
+LINE 02 9.6 CKT. #36FDPA009219-PDY TO ND7776 ; KH# 7820 ;L.N.02
+LINE 03 = (9.6 CKT.) #FDEA525350/PDY9600 TO ND7522 K.H. 8511 ; L.N.02
+LINE 04 = OPEN
+LINE 05 = OPEN
+LINE 06 = OPEN
+
+************************** PARADYNE SERIAL NUMBERS ****************************
+CKT # FDE?-----        ;S/N 2633    :CKT # FDEC92473        ;S/N 2088
+CKT. #36FDPA009219     ;S/N23076      ;ASSET TAG # 258351
+CKT #                  ;S/N         :CKT #                  ;S/N
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
