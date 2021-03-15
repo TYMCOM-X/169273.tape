732f4d6d0709d35f3c2305231201f570a04cc00b
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:29:46 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd3423.d
@@ -0,0 +1,61 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 3423   DISTRICT: DALLAS    DATE BLACKBOOK UPDATED: 11-13-86
+LAST PROJECT NUMBER:             DATE COMPLETED : CHANGE DUE TO RECONFIG
+FIELD ENGINEER CONTACT : JOHN HALE   DATE CREATED:MAY 15,1984
+
+******************************* L O C A T I O N *******************************
+CUSTOMER NAME: ALLIED ELECTRONICS     ADDRESS: 401 E. 8TH STREET
+CITY/STATE/ZIP:                                FT. WORTH,TX.
+CUSTOMER PHONE: 817/336-5401          TYMNET FE PHONE: 817/336-5401
+
+******************************* C O N T A C T S *******************************
+8-5PM M-F: ALLEN BRUNS, KEN MESSER  WORK: 817/336-5401 EXT. 252 FOR ALLEN
+*AFTER 5: JOHN                      TEL: 817/336-5401
+
+********************************* A C C E S S *********************************
+COMMENTS: ACCESS PERMITTED 24 HRS. CALL AHEAD TO TELL THEM YOU'RE
+          COMING IF AFTER 17:00.
+
+*******************************************************************************
+PVC OR PAP CODE REQUIRED? NO     HIGH SPEED ASYNC? YES
+*******************************************************************************
+
+******************************* H A R D W A R E *******************************
+MACHINE TYPE: FULL BLOWN ENGINE ;MACHINE CONFIG. TYPE: T-II ISIS TYMCOM
+KERNEL HOST # = 2137            ;M/F BOARD = LBOOT6 3POSITION
+FILE NAME = (TYMNET)ND3423.BND  ;FIRMWARE = VERSION 18 PROMS
+MAC TYPE = MAC II               ;MEMORY TYPE & AMOUNT = 1 MEG ZITEL
+NO. OF SYNC CARDS = 1           ;NO. OF ASYNC CARDS = 2
+NO. OF SIO MBS = 0              ;NO. OF SIO DBS = 0
+TOTAL PARADYNES = 1             ;MPX = 0        ;MP96 = 1
+NO. OF HOST SLOTS = 2           ;NO. OF DATATROLS = 0
+NO. OF VADICS = 0               ;NO. OF 202T'S = 0
+NO. OF CONNECTOR PANELS = 1     ;NO. OF MODEM BREAKOUT PANELS = 0
+FCO'S COMPLETE? YES             ;NO. OF ASYNC BREAKOUT PANELS = 0
+NO. OF PRINTERS = 0
+
+************************ PAP AND/OR PVC INFORMATION ***************************
+
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO: 214/823-4171       LOCAL: 214/824-1121
+
+************** N E T W O R K   N O D E   &   H O S T   L I N K S **************
+LINE 00 = (4.8)FDEA298231/PDY TO N-2046 K.H.1678 L.00
+LINE 01 = (4.8)16FDPA011758/PDY TO N-5327 K.H.6729 L.04
+LINE 02 = OPEN
+LINE 03 = OPEN
+LINE 04 = OPEN
+************** C U S T O M E R    I N T E R F A C E   S L O T S ***************
+SLOT 1   HOST#=2138 ;CUSTOMER= ALLIED ELECTRONICS
+ADDRESS: 401 E. 8TH STREET   ;CITY/STATE/ZIP: FOAT WUTH,TX.
+PHONE: 817/336-5401  ; CONTACT= OPERATOR       ; HOST TYPE:CP8A/2400
+L.U. = 0,1,2A.REF =0,1,2  ;S.REF =N/A   ;M.REF =N/A
+SLOT 2   HOST#=NONE ;CUSTOMER=NONE
+ADDRESS:                     ;CITY/STATE/ZIP:
+PHONE:               ; CONTACT=                ; HOST TYPE:
+L.U. =     ;A.REF =       ;S.REF =      ;M.REF =
+
+************************** PARADYNE SERIAL NUMBERS ****************************
+CKT # FDEA298231       ;S/N 9215    :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
