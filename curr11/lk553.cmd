2a8d6c8e7fad26903485a521ef660a5766d7f0de
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 08:03:56 +0000

    Files extracted from 169273.tape

---
+++ curr11/lk553.cmd
@@ -0,0 +1,10 @@
+SV553.IMG/img/b:1000,mp553=cg553,ru553,
+exec,error,pkt1,pkt2,pkt3d,XDT,
+cmdtym,CMDS,trm,trmf,TRMP,
+trmt,zapbuf,load,
+lnk1,lnk2,
+lnkflg,lnkb,lnkbs,lkbduv,crcbyt,
+LNKG,LKGPKT,LKGQAL,CRCTAB,
+LNKRSP,LNKC,STATUS,
+lina,linb,auto13,once,end
+
