cc461b648cb7739ee8e8e1f308ec0dfccfb56d97
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 08:00:41 +0000

    Files extracted from 169273.tape

---
+++ curr11/lk1376.cmd
@@ -0,0 +1,6 @@
+sv1376.IMG/img/b:21000,MP1376=Cg1376,ru1376,
+exec,error,pkt1,pkt2,pkt3d,
+cmdsat,cmdsst,trmst,trmfst,trmpst,
+trmtst,zapbuf,auto7,xltcrs,xltebc,
+lnk1,lnkha,lkhst1,lkhst2,lkhst3,
+tyma,linast,linbst,once,end
