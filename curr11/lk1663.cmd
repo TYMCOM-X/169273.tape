977a740a2910a8a72810e18d5ebeaa5659f0bfc5
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:57:19 +0000

    Files extracted from 169273.tape

---
+++ curr11/lk1663.cmd
@@ -0,0 +1,6 @@
+sv1663.img/img/b:21000=cg1663,ru1663,
+exec,error,pkt1,pkt2,pkt3d,
+cmdsat,cmdsst,trmst,trmfst,trmpst,
+trmtst,zapbuf,auto13,
+lnk1,lnk2,lnkha,lkhst1,lkhst2,lkhst3,
+tyma,linaST,linbST,once,end
