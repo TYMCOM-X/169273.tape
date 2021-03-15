b95140463c6d27581026090879257938665b19bb
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:35:12 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd7531.d
@@ -0,0 +1,41 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE:ND7531  DISTRICT: DALLAS    DATE BLACKBOOK UPDATED: NOV 13,1986
+LAST PROJECT NUMBER: 18427CDA001 DATE COMPLETED: NOV 02,1986
+FIELD ENGINEER CONTACT: BILL SCHNELLER
+****************************** L O C A T I O N ********************************
+CUSTOMER NAME: TYMNET INC.            ADDRESS: 6333 HARRY HINES BLVD
+CITY/STATE/ZIP: DALLAS,TX  75235
+CUSTOMER PHONE: 214/637-7316          TYMNET FE PHONE:
+******************************* C O N T A C T S *******************************
+8-5PM M-F: CNFED PERSONNEL          WORK:                   HOME:
+AFTER 5: STANDBY PERSON
+********************************* A C C E S S *********************************
+COMMENTS: THIS IS A MICRO. IF YOU REPLACE IT PLEASE CORRECT THE
+BLACKBOOK TO REFLECT THE NEW S/N AND THE NEW A/N.
+*******************************************************************************
+PVC OR PAP CODE REQUIRED? NO     HIGH SPEED ASYNC? NO
+******************************* H A R D W A R E *******************************
+SERIAL NUMBER: M860             ;ASSET TAG # 241107
+MACHINE TYPE: MICRO FULL MEG    ;MACHINE CONFIG. TYPE: ISIS
+KERNEL HOST # 3801              ;M/F BOARD = 
+FILE NAME = (TYM5)ND7531.BND    ;FIRMWARE =
+MAC TYPE =                      ;MEMORY TYPE & AMOUNT = 1024K SEMI
+NO. OF SYNC CARDS =             ;NO. OF ASYNC CARDS =
+NO. OF SIO MBS =                ;NO. OF SIO DBS =
+TOTAL PARADYNES =               ;MPX =          ;MP96 =
+NO. OF HOST SLOTS =             ;NO. OF DATATROLS =
+NO. OF VADICS =                 ;NO. OF 202T'S =
+NO. OF CONNECTOR PANELS =       ;NO. OF MODEM BREAKOUT PANELS =
+FCO'S COMPLETE?                 ;NO. OF ASYNC BREAKOUT PANELS =
+NO. OF PRINTERS =
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO: 214/823-4171       LOCAL: 214/824-1121
+************** N E T W O R K   N O D E   &   H O S T   L I N K S **************
+LINE 00 9.6 H/W LINK TO ND5330 ;KH# 6730 ;LN# 05
+LINE 01 9.6 H/W LINK TO ND5612 ;KH# 7139 ;LN# 05
+************** C U S T O M E R    I N T E R F A C E   S L O T S ***************
+SLOT 1   HOST#      ;CUSTOMER    
+ADDRESS:                     ;CITY/STATE/ZIP:                  
+PHONE:               ; CONTACT                 ; HOST TYPE:             
+L.U.       ;A.REF =       ;S.REF =      ;M.REF =
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
