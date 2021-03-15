ad7ffb2b7c67eb61f2296d649e0795d1c8e09642
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:30:07 +0000

    Files extracted from 169273.tape

---
+++ cnfe/sp2502.d
@@ -0,0 +1,58 @@
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 2502                 DISTRICT: DALLAS  86/02/25  07:42
+LAST PROJECT NUMBER : 
+FIELD ENGINEER CONTACT : COLEMAN RHOADES
+
+********************************************************************
+                                 **LOCATION**
+CUSTOMER NAME : SWB  ADDRESS : 611 W. ELM
+CITY/STATE/ZIP :                            TYLER
+CUSTOMER PHONE : 214-531-3400 TYMNET FE PHONE : -
+
+********************************************************************
+                                 **CONTACTS**
+8-5PM : CHARLIE BRIDGES    WORK : -
+
+AFTER 5 : -    TEL : -
+
+********************************************************************
+                                 **ACCESS COMMENTS**
+CALL CONTACT AT ENTRANCE TO BUILDING
+**
+
+
+
+********************************************************************
+PVC OR PAP CODE REQUIRED? NO  HIGH SPEED ASYNC? NO
+********************************************************************
+                                 **HARDWARE**
+SERIAL NUMBER: 0190
+MACHINE TYPE : MICRO  MACHINE CONFIG. TYPE: -
+KERNEL HOST # = 1014  FILENAME= SP2502
+ASSET TAG NUMBER = --
+
+MICRO-ENGINE PART NUMBER IS 650005-001 REV LEVEL B
+CONNECTOR PANEL  1 IS PART # AND REV LEVEL : CBB219 REV A
+THIS CONNECTOR PANEL IS S.N. = F9228-5014
+CONNECTOR PANEL  2 IS PART # AND REV LEVEL : CBB219 REV A
+THIS CONNECTOR PANEL IS S.N. = F9228-5011
+CONNECTOR PANEL  3 IS PART # AND REV LEVEL : CBB219 REV A
+THIS CONNECTOR PANEL IS S.N. = F9228-5016
+CONNECTOR PANEL  4 IS PART # AND REV LEVEL : CBB219 REV A
+THIS CONNECTOR PANEL IS S.N. = F9228-5015
+
+********************************************************************
+                                 **TELCO TROUBLE**
+TELCO TROUBLE NUMBER :0
+
+********************************************************************
+                                 **NETWORK LINKS**
+LINE     2502 = LINE 0  9.6  2501  1013  L-2
+LINE     2502 = LINE 1  9.6  2027  539  L-0
+LINE     - = -
+LINE     - = -
+LINE     - = -
+LINE     - = -
+LINE     - = -
+LINE     - = -
+********************************************************************
