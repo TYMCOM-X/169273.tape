e9d1cd428e7938ec7730b6b3e8c133c31f335ec4
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:29:37 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd5331.d
@@ -0,0 +1,53 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 5331   DISTRICT: DALLAS    DATE BLACKBOOK UPDATED: 11-18-86
+LAST PROJECT NUMBER: 11384.CD    DATE COMPLETED :
+FIELD ENGINEER CONTACT : JOHNNY JACKSON
+
+******************************* L O C A T I O N *******************************
+TYMNET SITE: TYMNET INC.              ADDRESS: 6333 HARRY HINES BLVD.
+CITY/STATE/ZIP: DALLAS,TEXAS 75235
+
+
+******************************* H A R D W A R E *******************************
+SERIAL NUMBER: 2760
+MACHINE TYPE: FULL BLOWN ENGINE ;MACHINE CONFIG. TYPE: SWITCHER
+KERNEL HOST # = 6731            ;M/F BOARD =
+FILE NAME = (TYM5)ND5331.BND    ;FIRMWARE =
+MAC TYPE = MAC III              ;MEMORY TYPE & AMOUNT = 1024 SEMI
+NO. OF SYNC CARDS = 1           ;NO. OF ASYNC CARDS = 0
+NO. OF SIO MBS = 1              ;NO. OF SIO DBS = 3
+TOTAL PARADYNES = 1             ;MPX = 1        ;MP96 =
+NO. OF HOST SLOTS = 0           ;NO, OF DATATROLS = 0
+NO. OF VADICS = 0               ;NO. OF 202T'S = 0
+NO. OF CONNECTOR PANELS = 1     ;NO. OF MODEM BREAKOUT PANELS = 1
+FCO'S COMPLETE?                 ;NO. OF ASYNC BREAKOUT PANELS = 0
+NO. OF PRINTERS = 0
+
+
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO: 214/823-4171       LOCAL: 214/824-1121
+
+********************** N E T W O R K   L I N K S ******************************
+LINE 00 = UNUSED
+LINE 01 = 9.6     N10254  KH#8203  (L-00)
+LINE 02 = 9.6     ND7334  KH#4152  (L-00)
+LINE 03 = 9.6     ND7224  KH#7809  (L-00)
+LINE 04 = 9.6     N10362  KH#8377  (L-00)
+LINE 05 = UNUSED
+LINE 06 = 9.6     N10557  KH#8763  (L-00)
+LINE 07 = 9.6     ND4120  KH#6603  (L-01)
+LINE 08 = UNUSED
+LINE 10 = MS     ND5326  KH#6728  (L-04)
+LINE 11 = MS     ND5612  KH#7139  (L-12)
+LINE 12 = MS     ND5377  KH#6846  (L-19)
+LINE 13 = MS     ND5330  KH#6730  (L-18)
+LINE 14 = MS     ND2152  KH#1363  (L-16)
+LINE 15 = MS     ND2046  KH#1678  (L-19)
+LINE 16 = MS     ND5617  KH#7162  (L-02)
+LINE 17 = MS     ND6275  KH#7866  (L-02)
+LINE 18 = MS     ND7451  KH#7865  (L-01)
+LINE 19 = MS     ND7450  KH#7864  (L-01)
+LINE 20 = UNUSED
+LINE 21 = UNUSED
+
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
