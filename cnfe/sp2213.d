b4e28ba130a62d14eda57e9ba284bb91e63bb6e4
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:38:47 +0000

    Files extracted from 169273.tape

---
+++ cnfe/sp2213.d
@@ -0,0 +1,36 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: SP2213 DISTRICT: DALLAS    DATE BLACKBOOK UPDATED: JUNE 10,1985
+LAST PROJECT NUMBER:             DATE COMPLETED :
+FIELD ENGINEER CONTACT :DAN MCLAUGHLIN 214/637-7416
+
+******************************* L O C A T I O N *******************************
+TYMNET SITE:SOUTHWESTERN BELL TELE.   ADDRESS:2111 S.AKARD ST.
+CITY/STATE/ZIP:DALLAS/TEXAS/75201
+CONTACTS:JOE CUTHBERTSON...PH#=214/464-5656
+         BOB PICKARD...PH#=214/464-5656
+******************************* H A R D W A R E *******************************
+MACHINE TYPE:MICRO-ENGINE       ;MACHINE CONFIG. TYPE:ISIS      
+KERNEL HOST # = 725            ;M/F BOARD = 3 POS SWT.
+FILE NAME = (SWBNET)SP2213.BND  ;FIRMWARE = VERSION 18 PROMS
+MAC TYPE = 11                   ;MEMORY TYPE & AMOUNT =1/2 MEG-ZITEL
+NO. OF SYNC CARDS = 1           ;NO. OF ASYNC CARDS =NONE
+NO. OF SIO MBS = 0              ;NO. OF SIO DBS =0
+TOTAL PARADYNES = 1             ;MPX = 1        ;MP96 =
+NO. OF HOST SLOTS =1            ;NO, OF DATATROLS =0
+NO. OF CONNECTOR PANELS =0      ;NO. OF MODEM BREAKOUT PANELS =0
+FCO'S COMPLETE? YES             ;NO. OF ASYNC BREAKOUT PANELS =0
+NO. OF PRINTERS =0
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO:                    LOCAL:214/824-1121
+
+********************** N E T W O R K   L I N K S ******************************
+LINE 00 =(19.2)H/W TO SP2026,K.H.538, L.02
+LINE 01 =(19.2)H/W TO SP2030,K.H.542, L.01
+LINE 02 =OPEN
+LINE 03 =OPEN
+LINE 04 =OPEN
+LINE 05 =OPEN
+************************** PARADYNE SERIAL NUMBERS ****************************
+CKT #                  ;S/N=        :CKT #                 ;S/N
+******************************************************************************
+MICRO-ENGINE S/N=0193
