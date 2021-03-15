6412aacb487d7f6a4c7f3475c997dea47f93d8ae
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:36:49 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd7334.d
@@ -0,0 +1,65 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 7334   DISTRICT: DALLAS    DATE BLACKBOOK UPDATED: JAN. 3,1986
+LAST PROJECT NUMBER: 15204       DATE COMPLETED : JAN. 3,1986
+FIELD ENGINEER CONTACT : RON BARDIN
+
+******************************* L O C A T I O N *******************************
+CUSTOMER NAME: EQUITABLE LIFE ASSURANCE SOC.
+LOCATION     : 4100 INTERNATIONAL PLAZA, 1ST. FLOOR
+CITY/STATE/ZIP: FT. WORTH, TEXAS  76109        NETWORK CONTROL
+CUSTOMER PHONE: 817/735-0572          TYMNET FE PHONE: NONE
+
+******************************* C O N T A C T S *******************************
+8-5PM M-F: LARRY RENTERIA           WORK: 817/735-0572
+           ANDY SMITH               WORK: SAME AS ABOVE
+*AFTER 5: ANYONE WHO MIGHT BE WORKING LATE AT SITE, IN NETWORK CONTROL.
+
+********************************* A C C E S S *********************************
+COMMENTS: CALL SITE BEFORE GOING OUT TO SITE.
+
+
+
+*******************************************************************************
+PVC OR PAP CODE REQUIRED? NO     HIGH SPEED ASYNC? NO  
+*******************************************************************************
+
+******************************* H A R D W A R E *******************************
+SERIAL NUMBER: 3270
+MACHINE TYPE: FULL ENGINE       ;MACHINE CONFIG. TYPE: ISIS-II
+KERNEL HOST # = 4152            ;M/F BOARD = PUSHBUTTON (BOOT)
+FILE NAME : (TYM4000)ND7334.BND ;FIRMWARE = VERSION 18 PROMS
+MAC TYPE = MAC II               ;MEMORY TYPE & AMOUNT = FULL MEG
+NO. OF SYNC CARDS = 1           ;NO. OF ASYNC CARDS = 1
+NO. OF SIO MBS = 1              ;NO. OF SIO DBS = 1
+TOTAL PARADYNES = 1             ;MPX = 1        ;MP96 = 0
+NO. OF HOST SLOTS = 3           ;NO. OF DATATROLS = 0
+NO. OF VADICS = 0               ;NO. OF 202T'S = 0
+NO. OF CONNECTOR PANELS = 0     ;NO. OF MODEM BREAKOUT PANELS = 0
+FCO'S COMPLETE? YES             ;NO. OF ASYNC BREAKOUT PANELS = 0
+NO. OF PRINTERS = 0             ;NOTE! THIS HAS A PSM (PORT SWITCH MODULE)
+                                 IN IT WITH 1 SYNC, AND 4 ASYNC BOARDS
+                                 AND 1 V.24 DAUGHTER BOARD.
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO: 817/338-6281       LOCAL: 214/824-1124
+
+************** N E T W O R K   N O D E   &   H O S T   L I N K S **************
+LINE 00 = 9.6 16FDPA011595-PDY TO ND5331 (K.H.#6731) LN. 03
+LINE 01 = UNUSED
+LINE 02 = UNUSED
+LINE 03 = UNUSED
+LINE 04 =
+************** C U S T O M E R    I N T E R F A C E   S L O T S ***************
+SLOT 1   HOST#= 6101; AHI-32/1200- EQUITABLE LIFE ASSURANCE SOC.
+ADDRESS:4100 INTERNATIONAL PLZ. ;CITY/STATE/ZIP: FT. WORTH, TEX. 76109
+PHONE:817/735-0572   ; CONTACT:LARRY RENTERIA  ; HOST TYPE:AHI-32/1200
+L.U. = 1   ;A.REF = 0,1   ;S.REF = N/A  ;M.REF = N/A
+SLOT 2  HOST #=     ;CUSTOMER=
+ADDRESS:                     ;CITY/STATE/ZIP:
+PHONE:               ; CONTACT=                ; HOST TYPE:
+L.U. =     ;A.REF =       ;S.REF =      ;M.REF =
+
+************************** PARADYNE SERIAL NUMBERS ****************************
+CKT # 16FDPA011595     ;S/N 255053   ;ASSETT TAG NO. 249425
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
