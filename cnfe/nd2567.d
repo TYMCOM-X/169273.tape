5c2695ec9aafc29418887d99f4a9724720e247f8
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:31:52 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd2567.d
@@ -0,0 +1,49 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+NODE: 2567   DISTRICT: DALLAS    DATE BLACKBOOK UPDATED: 30 JULY 1985
+LAST PROJECT NUMBER:               DATE COMPLETED:
+FIELD ENGINEER CONTACT : JOHNNY JACKSON
+
+******************************* L O C A T I O N *******************************
+
+TYMSHARE, INC.
+6333 HARRY HINES BLVD.
+DALLAS, TX 75235                      TYMNET FE PHONE: 214/637-7416
+
+*******************************************************************************
+PVC OR PAP CODE REQUIRED? NO     HIGH SPEED ASYNC? NO
+*******************************************************************************
+
+******************************* H A R D W A R E *******************************
+
+MACHINE TYPE: T-II ENGINE       ;MACHINE CONFIG. TYPE: T-II ISIS
+KERNEL HOST # = 2122            ;M/F BOARD = 3 POS SWT
+FILE NAME = (TYMNET)ND2567.BND  ;FIRMWARE = 17
+MAC TYPE = MAC II               ;MEMORY TYPE & AMOUNT = 1 MEG ZITEL
+NO. OF SYNC CARDS = 2           ;NO. OF ASYNC CARDS = 0
+NO. OF SIO MBS = 0              ;NO. OF SIO DBS = 0
+NO. OF HOST SLOTS = 2           ;NO. OF DATATROLS = 0
+NO. OF VADICS = 0               ;NO. OF 202T'S = 0
+NO. OF CONNECTOR PANELS = 2     ;NO. OF MODEM BREAKOUT PANELS = 2
+FCO'S COMPLETE?                 ;NO. OF ASYNC BREAKOUT PANELS = 0
+NO. OF PRINTERS = 0
+
+************************ TELCO TROUBLE NUMBERS ********************************
+
+TELCO: 214/823-4171       LOCAL: 214/824-1121
+
+************** N E T W O R K   N O D E   &   H O S T   L I N K S **************
+
+LINE 00 = (9.6)H/W TO ND5407 (K.HOST 6854 L2)
+LINE 01 = (9.6)H/W TO ND5405 (K.HOST 6852 L0)
+LINE 02 = (9.6)H/W TO ND5406 (K.HOST 6853 L1)
+
+******************** C U S T O M E R  I N T E R F A C E S *********************
+
+SLOT 1   HOST#= 3641; CUSTOMER = MARS PLUS SECONDARY MAG H.I.
+L.U. = 4   ;S1L0 S.REF(2);S1L1 S.REF(3);S1L2 S.REF(6);S1L3 S.REF(7)
+
+SLOT 2   HOST#= 3665;CUSTOMER= MARS PLUS SECONDARY MAG H.O.
+L.U. = 4   ;S2L0 S.REF(10);S2L1 S.REF(11);S2L2 S.REF(14);S2L3 S.REF(15)
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
