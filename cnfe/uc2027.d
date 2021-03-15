7a7af622c2a30c6976654739591cc81567b8c8de
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:37:47 +0000

    Files extracted from 169273.tape

---
+++ cnfe/uc2027.d
@@ -0,0 +1,43 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE:UC2027  DISTRICT: DALLAS    DATE BLACKBOOK UPDATED: MAY 3,1984     
+LAST PROJECT NUMBER:             DATE COMPLETED :
+FIELD ENGINEER CONTACT :TUCKER
+
+******************************* L O C A T I O N *******************************
+CUSTOMER NAME:UCC                     ADDRESS:1930 HI LINE DR.
+CITY/STATE/ZIP:DALLAS,TEXAS 75207
+CUSTOMER PHONE:214/655-8766           TYMNET FE PHONE:214/655-8766
+
+******************************* C O N T A C T S *******************************
+8-5PM M-F:BOB SMITH                 WORK:214/655-8766       HOME:214/268-1053
+                                    WORK:                   HOME:
+*AFTER 5:                           TEL:
+
+********************************* A C C E S S *********************************
+COMMENTS:PROBE=PLI:CNFE:1651;DOG GONE(CR),1STPW=BIGEYE,2NDPW=WELCOME
+         NAME=CCM.........XRAY PW=BIGEYE
+
+
+*******************************************************************************
+******************************* H A R D W A R E *******************************
+MACHINE TYPE:MINI-ENGINE        ;MACHINE CONFIG. TYPE:SWITCHER
+KERNEL HOST # =33               ;M/F BOARD =3 POS.SWITCH
+FILE NAME =(UCCPAC:39)ND2027.BND;FIRMWARE =VERSION PROMS ON ROM&I/O
+MAC TYPE =MAC II                ;MEMORY TYPE & AMOUNT =1/2 MEG ZITEL
+NO. OF SYNC CARDS =1 EA         ;NO. OF ASYNC CARDS =0
+NO. OF SIO MBS =0               ;NO. OF SIO DBS =0
+TOTAL PARADYNES =0              ;MPX =0         ;MP96 =0
+NO. OF CONNECTOR PANELS =1 EA
+FCO'S COMPLETE?YES              ;NO. OF ASYNC BREAKOUT PANELS =0
+************************ PAP AND/OR PVC INFORMATION ***************************
+              N O N E
+
+
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO:214/823-4171        LOCAL:214/824-1121
+
+************** N E T W O R K   N O D E   &   H O S T   L I N K S **************
+LINE 0 = (4.8)H/W TO N-2023,K.H.=29,LINE 1=LOAD STRING INFO.
+LINE 1 = OPEN
+LINE 2 = OPEN
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
