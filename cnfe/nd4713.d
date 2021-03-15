6a0cd45389bf52d977efb87ceea4ded3fd2a858e
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:34:27 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd4713.d
@@ -0,0 +1,66 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 4713   DISTRICT: DALLAS    DATE BLACKBOOK UPDATED: 14 AUG 1984
+LAST PROJECT NUMBER:             DATE COMPLETED :
+FIELD ENGINEER CONTACT : RON BREWER
+
+******************************* L O C A T I O N *******************************
+TYMNET SITE: TYMSHARE/MARS PLUS       ADDRESS: 6333 HARRY HINES BLVD.
+CITY/STATE/ZIP: DALLAS,TX. 75235
+
+
+******************************* H A R D W A R E *******************************
+MACHINE TYPE: TYMNET ENGINE     ;MACHINE CONFIG. TYPE: SWITCHER
+KERNEL HOST # = 5148            ;M/F BOARD = 3 POS. SWITCH
+FILE NAME = (TYMNET)ND4713.BND  ;FIRMWARE =
+MAC TYPE = MAC II               ;MEMORY TYPE & AMOUNT = SEMI (1024)
+NO. OF SYNC CARDS = 1           ;NO. OF ASYNC CARDS = 0
+NO. OF SIO MBS = 1              ;NO. OF SIO DBS = 2
+TOTAL PARADYNES = 0             ;MPX = 0        ;MP96 = 0
+NO. OF HOST SLOTS = 0           ;NO, OF DATATROLS = 0
+NO. OF VADICS = 0               ;NO. OF 202T'S = 0
+NO. OF CONNECTOR PANELS = 1     ;NO. OF MODEM BREAKOUT PANELS = 0
+FCO'S COMPLETE? YES             ;NO. OF ASYNC BREAKOUT PANELS = 0
+NO. OF PRINTERS = 0
+
+
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO: 214/823-4171       LOCAL: 214/824-1121
+
+********************** N E T W O R K   L I N K S ******************************
+LINE 00 = (9.6) H/W  ND4150 KH.6647 L-1
+LINE 01 = (9.6) TO ND4055 (K.HOST 3893;L 0)
+LINE 02 = (9.6) TO ND4057 (K.HOST 3895;L 0)
+LINE 03 = UNUSED
+LINE 04 = (9.6) TO ND4056 (K.HOST 3894;L 0)
+LINE 05 = (9.6) TO ND4060 (K.HOST 3896;L 0)
+LINE 06 = (9.6) TO ND4054 (K.HOST 3892;L 0)
+LINE 07 = (9.6) TO ND2254 (K.HOST 0269;L 0)
+LINE 08 = (9.6) H/W ND7073 KH.6681 L-1
+LINE 09 = (9.6) TO ND4053 (K.HOST 3891;L 1)
+LINE 10 = (9.6) TO ND2277 (K.HOST 1648;L 0)
+LINE 11 = (9.6) TO ND4714 (K.HOST 5149;L 1)
+LINE 12 = (9.6) TO ND2253 (K.HOST 0270;L 0)
+LINE 13 = (9.6) TO ND2656 (K.HOST 2237;L 1)
+LINE 14 = (9.6) TO ND2636 (K.HOST 2228;L 1)
+LINE 15 = (9.6) H/W  ND5673 KH.6649 L-1
+LINE 16 = MS TO ND4067 (K.HOST 3960;L 16)
+LINE 17 = OPEN
+LINE 18 = MS TO ND4070 (K.HOST 3961;L 18)
+LINE 19 = MS TO ND2043 (K.HOST 1749;L 20)
+LINE 20 = OPEN
+LINE 21 = OPEN
+LINE 22 = OPEN
+LINE 23 = OPEN
+
+************************** PARADYNE SERIAL NUMBERS ****************************
+CKT #                  ;S/N         :CKT #                 ;S/N
+CKT #                  ;S/N         :CKT #                 ;S/N
+CKT #                  ;S/N         :CKT #                 ;S/N
+CKT #                  ;S/N         :CKT #                 ;S/N
+CKT #                  ;S/N         :CKT #                 ;S/N
+CKT #                  ;S/N         :CKT #                 ;S/N
+CKT #                  ;S/N         :CKT #                 ;S/N
+CKT #                  ;S/N         :CKT #                 ;S/N
+CKT #                  ;S/N         :CKT #                 ;S/N
+CKT #                  ;S/N         :CKT #                 ;S/N
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
