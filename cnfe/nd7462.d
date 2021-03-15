c6ada4630ca33b749a73c6ffe1a03c8f22c02078
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:30:09 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd7462.d
@@ -0,0 +1,46 @@
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 7462                 DISTRICT: DALLAS  86/02/03  11:54
+LAST PROJECT NUMBER : 1537102
+FIELD ENGINEER CONTACT : JOHNNY JACKSON
+
+********************************************************************
+                                 **LOCATION**
+CUSTOMER NAME : TYMNET  ADDRESS : 2121 OLD HENDERSON HWY
+CITY/STATE/ZIP :                            TYLER
+CUSTOMER PHONE : 214/597-3737 TYMNET FE PHONE : 214/587-
+
+********************************************************************
+                                 **CONTACTS**
+8-5PM : DAVID HIGGINBOTHAM    WORK : 214/597-3737
+
+AFTER 5 : WHO-SO-EVER IS THERE      TEL : SAME
+
+********************************************************************
+                                 **ACCESS COMMENTS**
+CALL BEFORE DISPATCHING
+**
+
+
+
+********************************************************************
+PVC OR PAP CODE REQUIRED? NO  HIGH SPEED ASYNC? N
+********************************************************************
+                                 **HARDWARE**
+SERIAL NUMBER: NA
+MACHINE TYPE : MICRO  MACHINE CONFIG. TYPE: TYMSAT
+KERNEL HOST # = 2398  FILENAME= (TYM4000)ND7462.BND
+ASSET TAG NUMBER = NA
+
+MICRO-ENGINE PART NUMBER IS 570011-001 REV LEVEL NA
+
+THERE ARE 1 MP9600 MODEMS (600028-001)
+THERE ARE 7 VADICS (600044-001)
+
+********************************************************************
+                                 **TELCO TROUBLE**
+TELCO TROUBLE NUMBER :214/748-4115
+
+********************************************************************
+                                 **NETWORK LINKS**
+LINE     2 = 16FDPA404159 ND6014 KH1300 LN3
+********************************************************************
