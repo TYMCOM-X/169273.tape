a7a574ee0311a4d02d60afde96d0f218d66891f9
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:37:20 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd4024.d
@@ -0,0 +1,70 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 4024   DISTRICT: DALLAS    DATE BLACKBOOK UPDATED: 09/11/85
+LAST PROJECT NUMBER: 13664       DATE COMPLETED : 09/11/85
+FIELD ENGINEER CONTACT : RON BARDIN CNFED
+
+******************************* L O C A T I O N *******************************
+CUSTOMER NAME: TYMSHARE               ADDRESS: 6333 HARRY HINES, BLVD. COM. CEN.
+CITY/STATE/ZIP: DALLAS, TEX. 75235
+CUSTOMER PHONE: 214/637-4716          TYMNET FE PHONE: 214/637-7440
+
+******************************* C O N T A C T S *******************************
+8-5PM M-F: ALEX MARTEL              WORK: 214/637-7442      HOME:
+ 
+*AFTER 5: READ ACCESS COMMENTS.     TEL:
+
+********************************* A C C E S S *********************************
+COMMENTS: NODE 4024 IS IN THE TTMS AREA ON THE MAIN COMPUTER FLOOR IN A CABNET.
+THE SITE AS YOU KNOW, IS MANNED 24 HRS.
+*******************************************************************************
+PVC OR PAP CODE REQUIRED?        HIGH SPEED ASYNC? NO
+*******************************************************************************
+
+******************************* H A R D W A R E *******************************
+ASSETT NUMBER: 225749
+SERIAL NUMBER: M1472
+MACHINE TYPE: MICRO-II          ;MACHINE CONFIG. TYPE: T-II ISIS
+KERNEL HOST # = 6006            ;M/F BOARD = 
+FILE NAME = (TYM4000)ND4024.BND ;FIRMWARE = VERSION 18
+MAC TYPE = MAC II               ;MEMORY TYPE & AMOUNT = FULL MEG
+NO. OF SYNC CARDS = 1           ;NO. OF ASYNC CARDS = 1
+NO. OF SIO MBS = 0              ;NO. OF SIO DBS = 0
+TOTAL PARADYNES =               ;MPX =          ;MP96 = 0
+NO. OF HOST SLOTS = 3           ;NO. OF DATATROLS = 0
+NO. OF VADICS = 0               ;NO. OF 202T'S = 0
+NO. OF CONNECTOR PANELS = 0     ;NO. OF MODEM BREAKOUT PANELS = 0
+FCO'S COMPLETE?  ?              ;NO. OF ASYNC BREAKOUT PANELS = 0
+NO. OF PRINTERS = 0
+
+************************ PAP AND/OR PVC INFORMATION ***************************
+
+
+
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO:                    LOCAL: 214/824-1121
+
+************** N E T W O R K   N O D E   &   H O S T   L I N K S **************
+LINE 00 = 9.6 HW. TO NODE 4067, K.H.# 3960,  (LN. 11)
+LINE 01 = 9.6 HW. TO NODE 4070, K.H.# 3961,  (LN. 06)
+LINE 02 = NOT USED
+LINE XX = NOT USED
+LINE XX = NOT USED
+LINE XX = NOT USED
+LINE XX = NOT USED
+************** C U S T O M E R    I N T E R F A C E   S L O T S ***************
+SLOT 1   HOST# = 5273 ;CUSTOMER = MARS PLUS
+ADDRESS: 6333 HARRY HINES BLVD.  ;CITY/STATE/ZIP: DALLAS, TEX. 75235
+PHONE: 214/637-7442  ; CONTACT= ALEX MARTEL ;HOST TYP: SYNC POLLED T.I.
+L.U. = 2   ;A.REF = N/A   ;S.REF = (2,3) ;M.REF = N/A
+SLOT 2   HOST# = 5274 ;CUSTOMER = MARS PLUS
+ADDRESS: 6333 HARRY HINES BLVD. ;CITY/STATE/ZIP: DALLAS, TEX. 75235
+PHONE: 214/637-7442  ; CONTACT= ALEX MARTEL    ; HOST TYPE: SYNC POLLED T.I.
+L.U. = 2   ;A.REF =       ;S.REF = (6,7) ;M.REF =
+SLOT 3   HOST# = 1411 ;CUSTOMER = MARS PLUS
+ADDRESS: 6333 HARRY HINES BLVD. ;CITY/STATE/ZIP: DALLAS, TEX. 75235
+PHONE: 214/637-7442 ;CONTACT = ALEX MARTEL ;HOST TYPE: MEG SLOT
+L.U. = 3   ;A.REF =       ;S.REF = (14,15) ;M.REF =
+************************** PARADYNE SERIAL NUMBERS ****************************
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
