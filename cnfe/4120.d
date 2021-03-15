7b723a45e894694226eb0cfb438b7b6cbe770f37
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:31:10 +0000

    Files extracted from 169273.tape

---
+++ cnfe/4120.d
@@ -0,0 +1,59 @@
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 4120                 DISTRICT: DALLAS  86/08/12  12:45
+LAST PROJECT NUMBER : 18875
+FIELD ENGINEER CONTACT : DOUG MILLER
+
+********************************************************************
+                                 **LOCATION**
+CUSTOMER NAME : DRESSER ADDRESS : 17201 WATERVIEW
+CITY/STATE/ZIP :                            DALLAS/TX/5379
+CUSTOMER PHONE : 680-4100  TYMNET FE PHONE : 214/637-7440
+
+********************************************************************
+                                 **CONTACTS**
+8-5PM : AL KOZAR     WORK : 680-4100
+
+AFTER 5 : ANYONE IN COMPROOM     TEL : SAME
+
+********************************************************************
+                                 **ACCESS COMMENTS**
+PERSON AT DESK WILL LET YOU IN  AND CALL SOMEONE IN
+COMPROOM TO GET YOU
+**
+
+
+********************************************************************
+PVC OR PAP CODE REQUIRED? NP  HIGH SPEED ASYNC? NO
+********************************************************************
+                                 **HARDWARE**
+SERIAL NUMBER: M11852
+MACHINE TYPE : MICRO  MACHINE CONFIG. TYPE: ISIS
+KERNEL HOST # = 6603  FILENAME= (TYM5)ND4120
+ASSET TAG NUMBER = N/A
+
+MICRO-ENGINE PART NUMBER IS 11111 REV LEVEL REV E
+THERE ARE 2 MPX MODEMS (600052-001)
+THERE ARE  2 HOST SLOTS
+
+********************************************************************
+                                 **TELCO TROUBLE**
+TELCO TROUBLE NUMBER :824-1121
+
+********************************************************************
+                                 **NETWORK LINKS**
+LINE     10 = HW/10155/6602 0 (TYM5)ND4120.BND
+LINE     11 = 11FDPA029853/5331 7 (TYM5)ND4120.BND
+********************************************************************
+                            CUSTOMER INTERFACE SLOTS
+
+SLOT  1   HOST#= 6686   CUSTOMER= DRESSER
+ADDRESS: 17201 WATERVIEW   CITY/STATE/ZIP: DALLAS/TX/75379
+PHONE: SAME   CONTACT= SAME   HOST TYPE: SAME
+LOGICAL UNITS=  1  AREFS=  0  MREFS=  0  SREFS=  1
+
+SLOT  2   HOST#= 6687   CUSTOMER= DRESSER
+ADDRESS: 17201 WATERVIEW   CITY/STATE/ZIP: DALLAS TX/75379
+PHONE: SAME   CONTACT= SAME   HOST TYPE: CMT
+LOGICAL UNITS=  0  AREFS=  0  MREFS=  0  SREFS=  0
+
+********************************************************************
