a04532adb7959769b7b78c2de783e88fd84af122
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:29:30 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd5617.d
@@ -0,0 +1,47 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 5617   DISTRICT: DALLAS    DATE BLACKBOOK UPDATED: NOV 06,1986
+LAST PROJECT NUMBER: 18427CDA001 DATE COMPLETED :NOV 02,1986
+FIELD ENGINEER CONTACT : BILL SCHNELLER
+******************************* L O C A T I O N *******************************
+CUSTOMER NAME: TYMNET/MCDONNEL-DOUGLAS ADDRESS: 6333 HARRY HINES BLVD.
+CITY/STATE/ZIP: DALLAS,TEXAS 75235
+CUSTOMER PHONE: 214/637-7316          TYMNET FE PHONE:
+******************************* C O N T A C T S *******************************
+8-5PM M-F:CNFED PERSONNEL           WORK:
+AFTER 5: STANDBY PERSON
+********************************* A C C E S S *********************************
+COMMENTS:
+*******************************************************************************
+PVC OR PAP CODE REQUIRED? NO     HIGH SPEED ASYNC? NO
+******************************* H A R D W A R E *******************************
+SERIAL NUMBER: 2302             ASSET TAG NUMBER 249148
+MACHINE TYPE: ENGINE            ;MACHINE CONFIG. TYPE: T-II ISIS
+KERNEL HOST # = 7162            ;M/F BOARD = 3 POS. SW. LBOOT6
+FILE NAME =(TYM5)ND5617.BND     ;FIRMWARE = VERSION 18
+MAC TYPE = MACII                ;MEMORY TYPE & AMOUNT = 1 MEG ZITEL
+NO. OF SYNC CARDS = 1           ;NO. OF ASYNC CARDS = 0
+NO. OF SIO MBS = 1              ;NO. OF SIO DBS = 1 OCL
+TOTAL PARADYNES =               ;MPX =          ;MP96 =
+NO. OF HOST SLOTS = 2           ;NO. OF DATATROLS = 0
+NO. OF VADICS = 0               ;NO. OF 202T'S = 0
+NO. OF CONNECTOR PANELS = 1     ;NO. OF MODEM BREAKOUT PANELS = 1
+FCO'S COMPLETE?                 ;NO. OF ASYNC BREAKOUT PANELS = 0
+NO. OF PRINTERS = 0
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO: 214/823-4171       LOCAL: 214/824-1121
+************** N E T W O R K   N O D E   &   H O S T   L I N K S **************
+LINE 00 = UNUSED(XLINK)
+LINE 01 = MS       ND5327  KH#6729  (L-23)
+LINE 02 = MS       ND5331  KH#6731  (L-16)
+************** C U S T O M E R    I N T E R F A C E   S L O T S ***************
+SLOT 1   HOST#= 7729;CUSTOMER= KMART
+ADDRESS:                     ;CITY/STATE/ZIP:
+PHONE:               ; CONTACT=                ; HOST TYPE: SHI-3270/B
+L.U. = 1   ;A.REF =       ;S.REF = 2    ;M.REF =
+SLOT 2   HOST#= 7730;CUSTOMER= KMART
+ADDRESS:                     ;CITY/STATE/ZIP:
+PHONE:               ; CONTACT=                ; HOST TYPE: CMT SLOT
+L.U. = 0   ;A.REF =       ;S.REF =      ;M.REF =
+************************** PARADYNE SERIAL NUMBERS ****************************
+CKT # 11FDPA028513     ;S/N 254562  ;A/N 249231             ;CAB 3,64
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
