cfe6ce0af3278f78ee7504e282cceb72fe7c1634
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:32:37 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd7007.d
@@ -0,0 +1,68 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 7007   DISTRICT: DALLAS    DATE BLACKBOOK UPDATED:DECEMBER 2,1985
+LAST PROJECT NUMBER: 14678       DATE COMPLETED : DECEMBER 2,1985
+FIELD ENGINEER CONTACT : EDWIN D. IVY,SR.
+
+******************************* L O C A T I O N *******************************
+CUSTOMER NAME: AIRCO INDUSTRIES       ADDRESS: 901 E. VICKERY
+CITY/STATE/ZIP: FORT WORTH, TEXAS 76104 
+CUSTOMER PHONE: 817/332-3315          TYMNET FE PHONE: N/A
+
+******************************* C O N T A C T S *******************************
+8-5PM M-F: LAJOYA JONES             WORK: 817/332-3315      HOME:
+            JON PAKULSKI            WORK: 817/332-3315      HOME:
+*AFTER 5:                           TEL:
+
+********************************* A C C E S S *********************************
+COMMENTS: THE EQUIPMENT IS LOCATED IN A MOBILE HOME STYLE OFFICE BLDG.
+BEWARE OF THE DOBERMAN PINCHER GUARD DOG WHEN YOU OPEN THE FRONT DOOR.
+FOR YOUR OWN SAFETY, KNOCK ON THE DOOR AND ANNOUNCE YOURSELF.
+THIS IS AN 8AM TO 5PM OPERATION. NO ACCESS AFTER HOURS.
+*******************************************************************************
+PVC OR PAP CODE REQUIRED?        HIGH SPEED ASYNC?
+*******************************************************************************
+
+ASSET TAG #
+******************************* H A R D W A R E *******************************
+SERIAL NUMBER:
+MACHINE TYPE: ATC/4             ;MACHINE CONFIG. TYPE: TYMSAT
+KERNEL HOST # = 6806            ;M/F BOARD =
+FILE NAME =                     ;FIRMWARE =CODE BLOWN ONTO EPROMS
+MAC TYPE =                      ;MEMORY TYPE & AMOUNT =
+NO. OF SYNC CARDS =             ;NO. OF ASYNC CARDS =
+NO. OF SIO MBS =                ;NO. OF SIO DBS =
+TOTAL PARADYNES = 1             ;MPX = 1        ;MP96 =
+NO. OF HOST SLOTS =             ;NO. OF DATATROLS =
+NO. OF VADICS =                 ;NO. OF 202T'S =
+NO. OF CONNECTOR PANELS =       ;NO. OF MODEM BREAKOUT PANELS =
+FCO'S COMPLETE?                 ;NO. OF ASYNC BREAKOUT PANELS =
+NO. OF PRINTERS =
+
+************************ PAP AND/OR PVC INFORMATION ***************************
+
+
+
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO: 817/338-6281       LOCAL:
+
+************** N E T W O R K   N O D E   &   H O S T   L I N K S **************
+LINE 00 =
+LINE 01 = 12FDPA404297-PDY 4.8 TO NODE 5513,KH9679,L-6
+************** C U S T O M E R    I N T E R F A C E   S L O T S ***************
+SLOT 1   HOST#=     ;CUSTOMER=
+ADDRESS:                     ;CITY/STATE/ZIP:
+PHONE:               ; CONTACT=                ; HOST TYPE: 
+L.U. =     ;A.REF =       ;S.REF =      ;M.REF =
+SLOT 2   HOST#=     ;CUSTOMER=
+ADDRESS:                     ;CITY/STATE/ZIP:
+PHONE:               ; CONTACT=                ; HOST TYPE:
+L.U. =     ;A.REF =       ;S.REF =      ;M.REF =
+
+************************** PARADYNE SERIAL NUMBERS ****************************
+CKT #12FDPA404297      ;S/N 278994  :CKT #                  ;S/N
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+IF FOR SOME REASON THIS NODE GOES DOWN, PLEASE REFER TO (BLAIR:54)ATC OR
+(NTS:33)ATC.DOC FOR TROUBLESHOOTING PROCEDURES. TO RESTART AN ATC YOU CAN
+HAVE THE CUSTOMER SIMPLY CUT THE POWER OFF AND TURN IT BACK ON.  IF THIS
+FAILS OR YOU FEEL THAT YOU HAVE A LINE DOWN, THEN FOLLOW STANDARD PROCEDURES
+OF TROUBLESHOOTING.
