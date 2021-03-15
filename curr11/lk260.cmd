6c4c526c3fc89318e0ea429245c7ef45b93986fa
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 08:11:30 +0000

    Files extracted from 169273.tape

---
+++ curr11/lk260.cmd
@@ -0,0 +1,8 @@
+SV260.IMG/img/b:1000,MP260=CG260,RU260,
+execmm,error,pkt1,pkt2,pkt3d,
+cmdtym,cmds,trm,trmf,trmp,
+trmt,zapbuf,status,
+lnk1,lnk2,lnkf,LNKF1,
+LNKHA,LKHBS1,LKHBS2,LKHBS3,
+TYMA,TYMB,
+lina,ONCE,END
