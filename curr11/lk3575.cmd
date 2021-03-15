30b07752f56ac4b18e21ed248be3fec4d0e41084
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 08:26:09 +0000

    Files extracted from 169273.tape

---
+++ curr11/lk3575.cmd
@@ -0,0 +1,8 @@
+SV3575.IMG/img/b:1000,MP3575=CG3575,RU3575,
+exec,error,pkt1,pkt2,pkt3d,
+cmdtym,cmds,trm,trmf,trmp,
+trmt,zapbuf,status,
+lnk1,lnk2,lnkf,LNKF1,
+LNKIA,LKIBS1,LKIBS2,
+TYMA,TYMB,
+lina,ONCE,END
