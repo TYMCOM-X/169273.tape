d057d7590b776e44233a472e01df1e446a54b637
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:30:11 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd3664.d
@@ -0,0 +1,49 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+   
+NODE: 3664                       DATE BLACKBOOK UPDATED: 7 AUG.'85
+LAST PROJECT NUMBER:             DATE COMPLETED :
+FIELD ENGINEER CONTACT :DAN MCLAUGHLIN
+
+******************************* L O C A T I O N *******************************
+    
+TYMSHARE, INC.
+6333 HARRY HINES BLVD.
+DALLAS, TX. 75235                     PHONE: 214/637-7416
+
+******************************* C O N T A C T S *******************************
+     
+                    PHONE: 415/968-5465
+
+*******************************************************************************
+PVC OR PAP CODE REQUIRED? NO     HIGH SPEED ASYNC? NO
+*******************************************************************************
+
+******************************* H A R D W A R E *******************************
+     
+MACHINE TYPE: T-II ENGINE       ;MACHINE CONFIG. TYPE: SWITCHER
+KERNEL HOST # = 3435            ;M/F BOARD = 3 POS. SWT.
+FILE NAME = (TYMNET)ND3664      ;FIRMWARE = 17
+MAC TYPE = MACII                ;MEMORY TYPE & AMOUNT = 1024K SEMI
+NO. OF SYNC CARDS = 1           ;NO. OF ASYNC CARDS = 0
+NO. OF SIO MBS = 2              ;NO. OF SIO DBS = 2
+NO. OF CONNECTOR PANELS = 1     ;NO. OF MODEM BREAKOUT PANELS = 0
+FCO'S COMPLETE?                 ;NO. OF ASYNC BREAKOUT PANELS = 0
+
+************************ TELCO TROUBLE NUMBERS ********************************
+    
+TELCO: 214/823-4171       LOCAL: 214/824-1121
+
+********************** N E T W O R K   L I N K S ******************************
+    
+NODE 3664 LINE 16 (SAT. LINK) TO ND3663 KH=3433 L-16
+NODE 3664 LINE 17 OPEN
+NODE 3664 LINE 18 OPEN
+NODE 3664 LINE 19 OPEN
+NODE 3664 LINE 20 (SAT. LINK) TO ND3660 KH=3430 L-20
+NODE 3664 LINE 21 OPEN
+NODE 3664 LINE 22 MS TO ND2215 KH=1522 L-3
+NODE 3664 LINE 23 MS TO ND5405 KH=6852 L-17
+NODE 3664 LINE 24 MS TO ND2153 KH=1364 L-24
+NODE 3664 LINE 25 OPEN
+    
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
