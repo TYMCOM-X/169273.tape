a367dc3c80a21ed069b18551b1d9776cd38663eb
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:35:29 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd2700.d
@@ -0,0 +1,76 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 2700   DISTRICT: DALLAS    DATE BLACKBOOK UPDATED; 11/07/86
+LAST PROJECT NUMBER: 18427       DATE COMPLETED : 11/03/86
+FIELD ENGINEER CONTACT : JOHN HALE
+
+******************************* L O C A T I O N *******************************
+CUSTOMER NAME: TYMNET INC.            ADDRESS: 6333 HARY HINES BLVD.
+CITY/STATE/ZIP: DALLAS/TEXAS/75235
+CUSTOMER PHONE: 214/637-7416          TYMNET FE PHONE: 214/637-7302
+
+******************************* C O N T A C T S *******************************
+8-5PM M-F: JOHN HALE                WORK: 214/637-7302
+                                    WORK:                   HOME:
+*AFTER 5:                           TEL:
+
+********************************* A C C E S S *********************************
+COMMENTS:
+
+
+
+*******************************************************************************
+PVC OR PAP CODE REQUIRED?        HIGH SPEED ASYNC?
+*******************************************************************************
+
+******************************* H A R D W A R E *******************************
+SERIAL NUMBER:
+MACHINE TYPE: FULL ENGINE       ;MACHINE CONFIG. TYPE: ISIS II
+KERNEL HOST # = 6759            ;M/F BOARD = L BOOT 6
+FILE NAME = (TYMNET:70)ND2700.BND ;FIRMWARE = VERSION 18
+MAC TYPE = MAC II               ;MEMORY TYPE & AMOUNT = SEMI 1 MEG
+NO. OF SYNC CARDS = 1           ;NO. OF ASYNC CARDS =
+NO. OF SIO MBS = 1              ;NO. OF SIO DBS = 1
+TOTAL PARADYNES =               ;MPX =          ;MP96 =
+NO. OF HOST SLOTS = 5           ;NO. OF DATATROLS = 0
+NO. OF VADICS = 0               ;NO. OF 202T'S = 0
+
+FCO'S COMPLETE?                 ;NO. OF ASYNC BREAKOUT PANELS =
+NO. OF PRINTERS =
+
+************************ PAP AND/OR PVC INFORMATION ***************************
+
+
+
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO: 214 823-4171       LOCAL: 214/824-1121
+
+************** N E T W O R K   N O D E   &   H O S T   L I N K S **************
+LINE 00 = OPEN
+LINE 01 = MS TO N-5612 KH=7139 L-15
+LINE 02 = MS TO N-5330 KH=6730 L-13
+************** C U S T O M E R    I N T E R F A C E   S L O T S ***************
+SLOT 1   HOST#= 6267;CUSTOMER= TYMSHARE
+ADDRESS:                     ;CITY/STATE/ZIP:
+PHONE:               ; CONTACT=                ; HOST TYPE: 
+L.U. =     ;A.REF =       ;S.REF =      ;M.REF =
+SLOT 2   HOST#=6270 ;CUSTOMER= TYMSHARE
+ADDRESS:                     ;CITY/STATE/ZIP:
+PHONE:               ; CONTACT=                ; HOST TYPE:
+L.U. =     ;A.REF =       ;S.REF =      ;M.REF =
+SLOT 3   HOST#=6997  ;COSTOMER= TYMSHARE SSBA
+HOST TYPE: 3270
+S.REF (2)
+SLOT 4   HOST#=18652 ;COSTOMER = AMERICAN STERILIZER
+HOST TYPE= 3270
+S.REF (3)
+SLOT 5   HOST#=6241 ;COSTOMER= TYMSHARE PMTS
+HOST TYPE= 3270
+S.REF (6)
+************************** PARADYNE SERIAL NUMBERS ****************************
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
