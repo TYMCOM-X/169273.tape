a524397e366231020f99bc3563168aafd3a70568
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:32:46 +0000

    Files extracted from 169273.tape

---
+++ cnfe/sp2252.d
@@ -0,0 +1,43 @@
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 2252                 DISTRICT: DALLAS  86/03/28  08:54
+LAST PROJECT NUMBER : 
+FIELD ENGINEER CONTACT : RON BARDIN
+
+********************************************************************
+                                 **LOCATION**
+CUSTOMER NAME : SWB  ADDRESS : 102 N. ADAMS
+CITY/STATE/ZIP :                            ENID
+CUSTOMER PHONE : 405-242-6210 TYMNET FE PHONE : -
+
+********************************************************************
+                                 **CONTACTS**
+8-5PM : DALE DISPAIN    WORK : -
+
+AFTER 5 : -    TEL : 314-235-2696
+
+********************************************************************
+                                 **ACCESS COMMENTS**
+CALL BEFORE DISPATCHING
+**
+
+
+
+********************************************************************
+PVC OR PAP CODE REQUIRED? NO  HIGH SPEED ASYNC? NO
+********************************************************************
+                                 **HARDWARE**
+SERIAL NUMBER: 0596
+MACHINE TYPE : MICRO  MACHINE CONFIG. TYPE: -
+KERNEL HOST # = 734   FILENAME= SP2252.BND
+ASSET TAG NUMBER = -
+
+MICRO-ENGINE PART NUMBER IS 570011-001 REV LEVEL -
+
+********************************************************************
+                                 **TELCO TROUBLE**
+TELCO TROUBLE NUMBER :-
+
+********************************************************************
+                                 **NETWORK LINKS**
+LINE     0 = SP2252 LN00 9.6 TO 2037 09
+********************************************************************
