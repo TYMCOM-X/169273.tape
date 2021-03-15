02a7ece11afbc2ced2e68269fb0fdbf7b18d4b2c
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 08:11:05 +0000

    Files extracted from 169273.tape

---
+++ curr11/lk2344.cmd
@@ -0,0 +1,8 @@
+SV2344.IMG/img/b:1000,MP2344=CG2344,RU2344,
+exec,error,pkt1,pkt2,pkt3d,
+cmdtym,cmds,trm,trmf,trmp,
+trmt,zapbuf,status,
+lnk1,lnk2,lnkf,lnkf2,
+lnkia,lkibs1,lkibs2,
+TYMA,tymb,
+lina,once,end
