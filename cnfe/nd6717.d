e1ccd30a44e1b7d84ba6054b921f0a385269fc27
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:33:07 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd6717.d
@@ -0,0 +1,63 @@
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 6717                 DISTRICT: DALLAS  85/12/20  06:51
+LAST PROJECT NUMBER : 12630
+FIELD ENGINEER CONTACT : JOHN HALE
+
+********************************************************************
+                                 **LOCATION**
+CUSTOMER NAME : MCDONNELL DOUGLAS   ADDRESS : 6333 HARRY HINES BLVD
+CITY/STATE/ZIP :                            DALLAS/TX/75235
+CUSTOMER PHONE : 214/637-7435 TYMNET FE PHONE : 214/637-7440
+
+********************************************************************
+                                 **CONTACTS**
+8-5PM : SCOTT WILSON    WORK : 214/637-7440
+
+AFTER 5 : CNFEOPS    TEL : 214/637-7416
+
+********************************************************************
+                                 **ACCESS COMMENTS**
+COMPUTER CENTER OPEN 24 HOURS A DAY WITH CARD-ENTRY CARD.
+IF NO CARD- PLEASE PHONE AHEAD FOR ACCESS.
+**
+
+********************************************************************
+PVC OR PAP CODE REQUIRED? N   HIGH SPEED ASYNC? N
+********************************************************************
+                                 **HARDWARE**
+SERIAL NUMBER: 4437
+MACHINE TYPE : MINI-EBUS    MACHINE CONFIG. TYPE: ISIS
+KERNEL HOST # = 4437  FILENAME= (TYM4000:54)ND6717.BND
+REV LEVEL OF M/F = ASB252-1-12 (430118-001)
+REV LEVEL OF ROM I/O = ASB225-01-13 (430136-001)
+REV LEVEL OF CPU = K (430137-001)
+REV LEVEL OF MAC = 85-271 (430109-001)
+MEMORY PART NUMBER AND REV LEVEL = #430147-001 REV 85-476
+SYNC BOARD  1 IS REV LEVEL F (430111-001)
+THERE ARE  2 HOST SLOTS
+CONNECTOR PANEL  1 IS PART # AND REV LEVEL : ASP113-1
+
+********************************************************************
+                                 **TELCO TROUBLE**
+TELCO TROUBLE NUMBER :214/824-1121
+
+********************************************************************
+                                 **NETWORK LINKS**
+LINE     0 = HW 2350-1858-8
+LINE     1 = HW 2347-2125-4
+LINE     2 = HW 2304-1664-15
+LINE     3 = OPEN
+********************************************************************
+                            CUSTOMER INTERFACE SLOTS
+
+SLOT  1   HOST#= 72   CUSTOMER= MCDONNELL DOUGLAS
+ADDRESS: 6333 HARRY HINES BLVD   CITY/STATE/ZIP: DALLAS/TX/75235
+PHONE: 214/637-7435   CONTACT= TOM MARCONI   HOST TYPE: EBUS 
+LOGICAL UNITS=  1  AREFS=  0  MREFS=  0  SREFS=  1
+
+SLOT  2   HOST#= NONE   CUSTOMER= EBUS INDICATES "DOWN-NOT LOADED" IN DDT
+ADDRESS: SAME   CITY/STATE/ZIP: SAME
+PHONE: SAME   CONTACT= SAME   HOST TYPE: PDP-10 BASE
+LOGICAL UNITS=  0  AREFS=  0  MREFS=  0  SREFS=  0
+
+********************************************************************
