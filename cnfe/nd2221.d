46d318addffced57e27f80d89d3085963f5eecd2
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:33:21 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd2221.d
@@ -0,0 +1,78 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 2221   DISTRICT: DALLAS    DATE BLACKBOOK UPDATED:   MAY 07, 1985
+LAST PROJECT NUMBER:             DATE COMPLETED :
+FIELD ENGINEER CONTACT : MIKE HAMILTON
+
+******************************* L O C A T I O N *******************************
+CUSTOMER NAME: TYMSHARE               ADDRESS: 6333 HARRY HINES BLVD.
+CITY/STATE/ZIP:                                DALLAS TX. 75235
+CUSTOMER PHONE: 214/637-7416          TYMNET FE PHONE: 214/637-7414
+
+******************************* C O N T A C T S *******************************
+8-5PM M-F: CNFED                    WORK: 214/637-7416      HOME:
+*AFTER 5: CNFED SWINGSHIFT TILL 24:00 THEN ON-CALL F.E.   TEL. 214/637-7416
+
+********************************* A C C E S S *********************************
+COMMENTS: ACCESS BY CNFED 24 HRS. 7 DAYS A WEEK WITH CARDENTRY KEY.
+
+
+
+*******************************************************************************
+PVC OR PAP CODE REQUIRED? NO     HIGH SPEED ASYNC? NO
+*******************************************************************************
+
+******************************* H A R D W A R E *******************************
+MACHINE TYPE: T-II ISIS BASE    ;MACHINE CONFIG. TYPE: FULL BLOWN ENGINE
+KERNEL HOST # = 1527            ;M/F BOARD = LBOOT6 3 PST.
+FILE NAME = (TYMNET)ND2221.BND  ;FIRMWARE = VERSION 17 PROMS
+MAC TYPE = MAC I                ;MEMORY TYPE & AMOUNT = 512 4 AMPEX CORE
+NO. OF SYNC CARDS = 1           ;NO. OF ASYNC CARDS =0
+NO. OF SIO MBS = 1              ;NO. OF SIO DBS = 3
+TOTAL PARADYNES =0              ;MPX =0         ;MP96 =0
+NO. OF HOST SLOTS = 4           ;NO. OF DATATROLS = 0
+NO. OF VADICS = 0               ;NO. OF 202T'S =0
+NO. OF CONNECTOR PANELS = 1     ;NO. OF MODEM BREAKOUT PANELS =0
+FCO'S COMPLETE? YES             ;NO. OF ASYNC BREAKOUT PANELS =0
+NO. OF PRINTERS = 0
+
+************************ PAP AND/OR PVC INFORMATION ***************************
+
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO: 214/823-4171       LOCAL: 214/824-1121
+
+************** N E T W O R K   N O D E   &   H O S T   L I N K S **************
+LINE 00 = OPEN
+LINE 01 = OPEN
+LINE 02 = OPEN
+LINE 03 = OPEN
+LINE 16 = (MS) LEMCOM HOST 47
+LINE 17 = (MS) BACKUP FOR HOST 47
+LINE 18 = (MS) BACKUP FOR HOST 47
+LINE 19 = (MS) BACKUP FOR HOST 47
+LINE 20 = (MS) TO N-2304 K.H. 1664 L.17
+LINE 21 = (MS) TO N-2347 K.H. 2125 L.17
+LINE 22 = OPEN
+************** C U S T O M E R    I N T E R F A C E   S L O T S ***************
+SLOT 1   HOST#=47   ;CUSTOMER= TYMSHARE
+ADDRESS: 6333 HARRY HINES    ;CITY/STATE/ZIP: DALLAS TX. 75235
+PHONE:214/637-7436   ; CONTACT= OPERATOR       ; HOST TYPE: LEMCOM
+L.U. =0    ;A.REF =N/A    ;S.REF =N/A   ;M.REF = 0,0
+SLOT 2   HOST#=47   ;CUSTOMER= SAME AS ABOVE
+ADDRESS: SAME AS ABOVE       ;CITY/STATE/ZIP: SAME AS ABOVE
+PHONE: SAME AS ^^    ; CONTACT= OPERATOR       ; HOST TYPE: SAME
+L.U. = 0   ;A.REF =N/A    ;S.REF =N/A   ;M.REF = 0,1
+
+SLOT 3   HOST#=2688 ;CUSTOMER= TYMSHARE
+ADDRESS: SAME AS ABOVE       ;CITY/STATE/ZIP:
+PHONE: SAME          ; CONTACT= OPERATOR       ; HOST TYPE:SAME
+L.U. = 0   ;A.REF =N/A    ;S.REF =N/A   ;M.REF = 0,2
+SLOT 4   HOST#=47   ;CUSTOMER=SAME
+ADDRESS: SAME                ;CITY/STATE/ZIP:SAME
+PHONE:SAME           ; CONTACT=OPERATOR        ; HOST TYPE:SAME
+L.U. =0    ;A.REF =N/A    ;S.REF =N/A   ;M.REF = 0,3
+
+************************** PARADYNE SERIAL NUMBERS ****************************
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
