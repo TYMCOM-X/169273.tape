4a017d9cced2aca731b9de038372e717dc49a2a2
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 08:04:14 +0000

    Files extracted from 169273.tape

---
+++ curr11/lksri1.cmd
@@ -0,0 +1,7 @@
+SVSRI1.IMG/img/b:1000,mpSRI1=cgSRI1,ruSRI1,
+exec,error,pkt1,pkt2,pkt3d,
+cmdtym,CMDS,trm,trmf,TRMP,
+auto13,trmt,zapbuf,
+lnk1,lnk2,lnkf,lnkf1,TYMA,tymb,
+lina,linb,status,once,end
+
