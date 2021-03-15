0c183b6dcd79a5524aabdc4d3ba55245d2db3a9d
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:31:59 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd6777.d
@@ -0,0 +1,64 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 6777   DISTRICT: DALLAS    DATE BLACKBOOK UPDATED: JUNE 6,1985
+LAST PROJECT NUMBER: 12776       DATE COMPLETED : JUNE 6,1985
+FIELD ENGINEER CONTACT : RON BARDIN
+
+******************************* L O C A T I O N *******************************
+CUSTOMER NAME: MARCON INDUSTRIES      ADDRESS: 4733 FLETCHER AVE.
+CITY/STATE/ZIP: FT. WORTH, TX.                 1ST FL., COMP. ROOM
+CUSTOMER PHONE: 817/731-7439          TYMNET FE PHONE: 214/637-7440
+
+******************************* C O N T A C T S *******************************
+8-5PM M-F: W. B. LEEDS              WORK: 817/731-7439
+           BUDDY MARTIN             WORK: SAME AS ABOVE
+*AFTER 5: ANYONE WHO MIGHT BE WORKING LATE AT SITE.
+
+********************************* A C C E S S *********************************
+COMMENTS: CALL SITE BEFORE GOING OUT TO SITE.
+
+
+
+*******************************************************************************
+PVC OR PAP CODE REQUIRED? NO     HIGH SPEED ASYNC? NO  
+*******************************************************************************
+
+******************************* H A R D W A R E *******************************
+SERIAL NUMBER: M792
+MACHINE TYPE: MICRO             ;MACHINE CONFIG. TYPE: ISIS-II
+KERNEL HOST # = 4997            ;M/F BOARD = PUSHBUTTON (BOOT)
+FILE NAME = (TYM4000)ND6777.TYM ;FIRMWARE = VERSION 18 PROMS
+MAC TYPE = MAC II               ;MEMORY TYPE & AMOUNT = 512 SEMI
+NO. OF SYNC CARDS = 1           ;NO. OF ASYNC CARDS = 1
+NO. OF SIO MBS = 0              ;NO. OF SIO DBS = 0
+TOTAL PARADYNES = 1             ;MPX = 1        ;MP96 = 0
+NO. OF HOST SLOTS = 1           ;NO. OF DATATROLS = 0
+NO. OF VADICS = 0               ;NO. OF 202T'S = 0
+NO. OF CONNECTOR PANELS = 0     ;NO. OF MODEM BREAKOUT PANELS = 0
+FCO'S COMPLETE? YES             ;NO. OF ASYNC BREAKOUT PANELS = 0
+NO. OF PRINTERS = 0
+
+
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO: 817/338-6281       LOCAL: 817/824-1121
+
+************** N E T W O R K   N O D E   &   H O S T   L I N K S **************
+LINE 00 = UNUSED
+LINE 01 = UNUSED
+LINE 02 = 4.8 12FDPA404215-PDY TO ND3645 (K.H.#3398;L-15)
+LINE 03 = UNUSED
+LINE 04 =
+************** C U S T O M E R    I N T E R F A C E   S L O T S ***************
+SLOT 1   HOST#= 4861; AHI-8/1200- MARCON INDUSTRIES
+ADDRESS:4733 FLETCHER AVE.   ;CITY/STATE/ZIP: FT. WORTH, TX.
+PHONE:817/731-7439   ; CONTACT: W.B. LEEDSS    ; HOST TYPE:AHI-8/1200
+L.U. = 1   ;A.REF = 0     ;S.REF = N/A  ;M.REF = N/A
+SLOT 2   HOST#=     ;CUSTOMER=
+ADDRESS:                     ;CITY/STATE/ZIP:
+PHONE:               ; CONTACT=                ; HOST TYPE:
+L.U. =     ;A.REF =       ;S.REF =      ;M.REF =
+
+************************** PARADYNE SERIAL NUMBERS ****************************
+CKT # 12FDPA404215     ;S/N 126283  :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
