17194562cff2652e149b53100610d9e1dde29d22
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:34:40 +0000

    Files extracted from 169273.tape

---
+++ cnfe/sp2532.d
@@ -0,0 +1,48 @@
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 2532                 DISTRICT: DALLAS  86/03/06  08:15
+LAST PROJECT NUMBER : 
+FIELD ENGINEER CONTACT : EDWIN D. IVY
+
+********************************************************************
+                                 **LOCATION**
+CUSTOMER NAME : SWB  ADDRESS : 2 BELL PLAZA
+CITY/STATE/ZIP :                            DALLAS
+CUSTOMER PHONE : -   TYMNET FE PHONE : -
+
+********************************************************************
+                                 **CONTACTS**
+8-5PM : SETH WOOD    WORK : 214-464-5482
+
+AFTER 5 : -    TEL : -
+
+********************************************************************
+                                 **ACCESS COMMENTS**
+-
+**
+
+
+
+********************************************************************
+PVC OR PAP CODE REQUIRED? NO  HIGH SPEED ASYNC? YES
+********************************************************************
+                                 **HARDWARE**
+SERIAL NUMBER: -
+MACHINE TYPE : MINI   MACHINE CONFIG. TYPE: -
+KERNEL HOST # = -  FILENAME= -
+ASSET TAG NUMBER = -
+REV LEVEL OF M/F = J (430118-001) S.N.= 1151-0031
+REV LEVEL OF ROM I/O = L (430136-001) S.N.= 0111-0020
+REV LEVEL OF CPU = K (430137-001) S.N.= 1115-0065
+REV LEVEL OF MAC = L (430109-001) S.N.= OV0115-0056
+MEMORY PART NUMBER AND REV LEVEL = D S.N.= 1144-0013
+SYNC BOARD  1 IS REV LEVEL OV0104-0073 (430111-001) S.N.= -
+SYNC BOARD  2 IS REV LEVEL - (430111-001) S.N.= 2516-0050
+ASYNC BOARD  1 IS REV LEVEL - (430110-001) S.N.= 2649-0034
+
+********************************************************************
+                                 **TELCO TROUBLE**
+TELCO TROUBLE NUMBER :-
+
+********************************************************************
+                                 **NETWORK LINKS**
+********************************************************************
