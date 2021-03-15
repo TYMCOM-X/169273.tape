128325fd31a4fefd703551f112914676130cfd30
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:54:33 +0000

    Files extracted from 169273.tape

---
+++ curr11/lk566.cmd
@@ -0,0 +1,7 @@
+SV566.IMG/img/b:1000,MP566.MAP=CG566,RU566,
+exec,error,pkt1,pkt2,pkt3d,
+cmdtym,cmds,trm,trmf,trmp,
+trmt,zapbuf,status,
+lnk1,lnk2,lnkf,lnkf2,
+lnkha,lkhbs1,lkhbs2,lkhbs3,TYMA,tymb,
+lina,once,end
