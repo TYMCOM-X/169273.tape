f346751d15cab9d57dea744ca566af0d89dc4d4f
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 08:03:18 +0000

    Files extracted from 169273.tape

---
+++ curr11/lk364.cmd
@@ -0,0 +1,7 @@
+SV364.IMG/img/b:1000,MP364.MAP=CG364.OBJ,RU364.OBJ,
+exec.OBJ,error.OBJ,pkt1.OBJ,pkt2.OBJ,pkt3d.OBJ,
+cmdtym.OBJ,cmds.OBJ,trm.OBJ,trmf.OBJ,trmp.OBJ,
+trmt.OBJ,zapbuf.OBJ,status.OBJ,
+lnk1.OBJ,lnk2.OBJ,lnkf.OBJ,LNKF1.OBJ,
+LNKHA,LKHBS1,LKHBS2,LKHBS3,TYMA,TYMB,
+lina.OBJ,ONCE,END
