0e46cc302e436e15f8eb3e43bed4120785e24ef0
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:32:43 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd3047.d
@@ -0,0 +1,61 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 3047   DISTRICT: DALLAS    DATE BLACKBOOK UPDATED: AUG. 7,1985
+LAST PROJECT NUMBER:             DATE COMPLETED :
+FIELD ENGINEER CONTACT : MARK ROGACZEWSKI
+
+******************************* L O C A T I O N *******************************
+CUSTOMER NAME: DAVID COOKE & ASSOC.   ADDRESS: 6510 ABRAMS RD.,SUITE 410
+CITY/STATE/ZIP:                                DALLAS,TX. 75231
+CUSTOMER PHONE: 214/349-6900          TYMNET FE PHONE: ?
+
+******************************* C O N T A C T S *******************************
+8-5PM M-F: RUSSELL GOLDEN           WORK: 214/349-6900
+*AFTER 5: UNMANNED AFTER FIVE!      TEL:
+
+********************************* A C C E S S *********************************
+COMMENTS: BUILDING ACCESSABLE ONLY FROM 08:00 TO 17:00. NO MAINTENANCE
+          REQUIRED AFTER 17:00.
+
+*******************************************************************************
+PVC OR PAP CODE REQUIRED? NO     HIGH SPEED ASYNC? NO
+*******************************************************************************
+
+******************************* H A R D W A R E *******************************
+MACHINE TYPE: MICRO ENGINE      ;MACHINE CONFIG. TYPE: T-I,TYMCOM ISIS
+KERNEL HOST # = 2621            ;M/F BOARD = BOOT PUSHBUTTON  
+FILE NAME = (TYMNET)ND3047.BND  ;FIRMWARE = VERSION 17 PROMS
+MAC TYPE = MAC II               ;MEMORY TYPE & AMOUNT = 512K ZITEL
+NO. OF SYNC CARDS = 1           ;NO. OF ASYNC CARDS = 1
+NO. OF SIO MBS = 0              ;NO. OF SIO DBS = 0
+TOTAL PARADYNES = 1             ;MPX =0         ;MP96 = 1
+NO. OF HOST SLOTS = 2           ;NO. OF DATATROLS = 0
+NO. OF VADICS = 0               ;NO. OF 202T'S = 0
+NO. OF CONNECTOR PANELS = 1     ;NO. OF MODEM BREAKOUT PANELS =0
+FCO'S COMPLETE? YES             ;NO. OF ASYNC BREAKOUT PANELS = 0
+NO. OF PRINTERS = 0
+
+************************ PAP AND/OR PVC INFORMATION ***************************
+
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO: 214/823-4171       LOCAL: 214/824-1121
+
+************** N E T W O R K   N O D E   &   H O S T   L I N K S **************
+LINE 00 = (4.8)11FDDA024992/PDY TO N-2401 K.H. 1840 L. 0
+LINE 01 = OPEN
+LINE 02 = OPEN
+LINE 03 = OPEN
+LINE 04 = OPEN
+************** C U S T O M E R    I N T E R F A C E   S L O T S ***************
+SLOT 1   HOST#=2622 ;CUSTOMER= DAVID COOK & ASSOC.
+ADDRESS: SAME AS ABOVE       ;CITY/STATE/ZIP: SAME AS ABOVE
+PHONE: SAME          ; CONTACT=                ; HOST TYPE: CP16A/1200
+L.U. = 0,1 ;A.REF = 0,1   ;S.REF =N/A   ;M.REF =N/A
+SLOT 2   HOST#=0000 ;CUSTOMER= NONE
+ADDRESS:                     ;CITY/STATE/ZIP:
+PHONE:               ; CONTACT=                ; HOST TYPE:
+L.U. =     ;A.REF =       ;S.REF =      ;M.REF =
+
+************************** PARADYNE SERIAL NUMBERS ****************************
+CKT # 11FDDA024992     ;S/N 10605   :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
