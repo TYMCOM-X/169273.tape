16e785a65e6c5134e09f57405144ee710f9678b0
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:31:20 +0000

    Files extracted from 169273.tape

---
+++ cnfe/va2231.d
@@ -0,0 +1,87 @@
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 2231                 DISTRICT: DALLAS  86/05/20  08:31
+LAST PROJECT NUMBER : 13182.30
+FIELD ENGINEER CONTACT : ED DOBBS
+
+********************************************************************
+                                 **LOCATION**
+CUSTOMER NAME : VETERANS ADMINISTRATION   ADDRESS : 510 E. STONER AVE.
+CITY/STATE/ZIP :                            SHREVEPORT LA. 71130
+CUSTOMER PHONE : 318/424-6150 TYMNET FE PHONE : 214/637-7440
+
+********************************************************************
+                                 **CONTACTS**
+8-5PM : EVAN NELSON     WORK : 318/424-6150
+        SHIRLEY THURSTON     : 318/424-6150
+
+AFTER 5 : NONE    TEL : NONE
+
+********************************************************************
+                                 **ACCESS COMMENTS**
+ACCESS BETWEEN 08:00 TO 16:30  MON. - FRI. ONLY
+**
+
+
+
+********************************************************************
+PVC OR PAP CODE REQUIRED? N   HIGH SPEED ASYNC? YES
+********************************************************************
+                                 **HARDWARE**
+SERIAL NUMBER: 1411 11/85
+MACHINE TYPE : MINI   MACHINE CONFIG. TYPE: ISIS
+KERNEL HOST # = 1231  FILENAME= (VACODE:26)VA2231.BND
+ASSET TAG NUMBER = 226457
+REV LEVEL OF M/F = M2 (430118-001) S.N.= B328
+REV LEVEL OF ROM I/O = L & M2 (430136-001) S.N.= B443
+REV LEVEL OF CPU = L (430137-001) S.N.= 0006
+REV LEVEL OF MAC = F1 & S1 (430109-001) S.N.= B354
+MEMORY PART NUMBER AND REV LEVEL = 430147-001  F1 S.N.= B210
+SYNC BOARD  1 IS REV LEVEL H1 (430111-001) S.N.= B104
+ASYNC BOARD  1 IS REV LEVEL A1 (430110-001) S.N.= OVO625-0018
+SIO MOTHER BOARD  1 IS REV LEVEL L2 (430123-001) S.N.= B126
+SIO DAUGHTER BD.  1 IS A V24 (430128-001) S.N.= 0096
+THERE ARE  4 HOST SLOTS
+
+********************************************************************
+                                 **TELCO TROUBLE**
+TELCO TROUBLE NUMBER :.
+
+********************************************************************
+                                 **NETWORK LINKS**
+LINE     0 = FDEA291382/2505 1505 3
+LINE     2 = S.REF FOR TI-SOFT EQUIPMENT
+********************************************************************
+                                  **PSM INFO**
+ONE MAIN CARD CAGE (650015-001)
+THERE IS A CONTROL CARD (430208-001) REV= . S.N.= .
+THIS 4 CHANNEL SYNC UNSWITCHED NON-SIO CARD (430193-001) IS REV= . S.N.= .
+THIS 4 CHANNEL SYNC UNSWITCHED NON-SIO CARD (430193-001) IS REV= . S.N.= .
+THIS 8 CHANNEL ASYNC UNSWITCHED NON-SIO CARD (430192-001) IS REV= . S.N.= .
+THIS 8 CHANNEL ASYNC UNSWITCHED NON-SIO CARD (430192-001) IS REV= . S.N.= .
+THIS 8 CHANNEL ASYNC UNSWITCHED NON-SIO CARD (430192-001) IS REV= . S.N.= .
+THIS 8 CHANNEL ASYNC UNSWITCHED NON-SIO CARD (430192-001) IS REV= . S.N.= .
+THIS 4 CHANNEL SYNC RS232 SIO UNSWITCHED CARD (430212-001) IS REV= . S.N.= 0096
+********************************************************************
+                            CUSTOMER INTERFACE SLOTS
+
+SLOT  1   HOST#= 477   CUSTOMER= VA
+ADDRESS: SAME AS ABOVE   CITY/STATE/ZIP: .
+PHONE: .   CONTACT= .   HOST TYPE: 2780T
+LOGICAL UNITS=  14  AREFS=  0  MREFS=  0  SREFS=  14
+
+SLOT  2   HOST#= 22312   CUSTOMER= VA
+ADDRESS: SAME AS ABOVE   CITY/STATE/ZIP: .
+PHONE: .   CONTACT= .   HOST TYPE: CONSAT
+LOGICAL UNITS=  1  AREFS=  1  MREFS=  0  SREFS=  0
+
+SLOT  3   HOST#= 835 & 722   CUSTOMER= VA
+ADDRESS: .   CITY/STATE/ZIP: .
+PHONE: .   CONTACT= .   HOST TYPE: TYMCOM
+LOGICAL UNITS=  1  AREFS=  1  MREFS=  0  SREFS=  0
+
+SLOT  4   HOST#= 836   CUSTOMER= VA
+ADDRESS: .   CITY/STATE/ZIP: .
+PHONE: .   CONTACT= .   HOST TYPE: X25
+LOGICAL UNITS=  4  AREFS=  0  MREFS=  4  SREFS=  0
+
+********************************************************************
