ac2b71e5baceead21246ce60acb6be77c3411097
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 08:20:25 +0000

    Files extracted from 169273.tape

---
+++ curr11/lk554a.cmd
@@ -0,0 +1,11 @@
+SV2770/IMG/B:1000,MP2770=CG2770.NBJ,RU2770.NBJ,
+exec.nbj,error.nbj,pkt1.nbj,pkt2.nbj,pkt3d.nbj,
+cmdtym.nbj,CMDS.nbj,trm.nbj,trmf.nbj,trmp.nbj,
+trmt.nbj,zapbuf.nbj,auto13.nbj,
+lnk1.nbj,lnk2.nbj,
+lnkf.nbj,LNKRSP.NBJ,LNKC.NBJ,
+lnkf2.nbj,LOAD.NBJ,
+lnkiap.nbj,lkibs1.nbj,lkibs2.nbj,
+TYMA.nbj,tymb.nbj,
+lina.nbj,linb.nbj,status.nbj,once.nbj,end.nbj
+
