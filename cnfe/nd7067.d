2b226b99d2fc001f9f63cc9876f89415432e402e
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:30:42 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd7067.d
@@ -0,0 +1,47 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE:7067    DISTRICT: DALLAS    DATE BLACKBOOK UPDATED: NOV 07,1986
+LAST PROJECT NUMBER: 18427CDA001DATE COMPLETED: NOV 02,1986
+FIELD ENGINEER CONTACT: BILL SCHNELLER
+******************************** L O C A T I O N ******************************
+CUSTOMER NAME:TYMNET                  ADDRESS:6333 HARRY HINES
+CITY/STATE/ZIP:                               DALLAS,TX,75235
+CUSTOMER PHONE:214/637-7316           TYMNET FE PHONE:
+******************************* C O N T A C T S *******************************
+8-5PM M-F:CNFED PERSONNEL           WORK:
+AFTER 5: STANDBY PERSON
+********************************* A C C E S S *********************************
+COMMENTS: THIS IS A MICRO. IF YOU REPLACE IT PLEASE CORRECT THIS
+BLACKBOOK TO SHOW THE S/N AND THE NEW A/N.
+******************************************************************************
+PVC OR PAP CODE REQUIRED?NO      HIGH SPEED ASYNC?NO
+******************************* H A R D W A R E *******************************
+SERIAL NUMBER: M1033            ASSET TAG NUMBER 241328
+MACHINE TYPE:MICRO              ;MACHINE CONFIG. TYPE: T-II ISIS
+KERNEL HOST # =4390             ;M/F BOARD =
+FILE NAME =(TYM5)ND7067.BND     ;FIRMWARE =
+MAC TYPE =                      ;MEMORY TYPE & AMOUNT =SEMI 1024K
+NO. OF SYNC CARDS =             ;NO. OF ASYNC CARDS =
+NO. OF SIO MBS =                ;NO. OF SIO DBS =
+TOTAL PARADYNES =               ;MPX =          ;MP96 =
+NO. OF HOST SLOTS = 2           ;NO. OF DATATROLS =
+NO. OF VADICS =                 ;NO. OF 202T'S =
+NO. OF CONNECTOR PANELS =       ;NO. OF MODEM BREAKOUT PANELS =
+FCO'S COMPLETE?                 ;NO. OF ASYNC BREAKOUT PANELS =
+NO. OF PRINTERS =
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO:214/823-4171        LOCAL:214/824-1121
+************** N E T W O R K   N O D E   &   H O S T   L I N K S **************
+LINE 00 = (9.6) H/W TO ND5612 KH7139 LINE 0  
+LINE 01 = (9.6) H/W TO ND5330 KH6730 LINE 0
+************** C U S T O M E R    I N T E R F A C E   S L O T S ***************
+SLOT 1   HOST#=19152;CUSTOMER= MDC
+ADDRESS:                     ;CITY/STATE/ZIP:DALLAS,TEXAS 75235  
+PHONE:               ; CONTACT=                ; HOST TYPE: STI 3270B
+L.U. =     ;A.REF =       ;S.REF = 6    ;M.REF =
+SLOT 2   HOST#=8429 ;CUSTOMER= MDC-ACS
+               8712
+ADDRESS:                     ;CITY/STATE/ZIP:
+PHONE:               ; CONTACT=                ; HOST TYPE: MENU SERVER
+L.U. = (CODE ONLY)
+************************** PARADYNE SERIAL NUMBERS ****************************
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
