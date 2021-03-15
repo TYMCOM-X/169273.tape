3ec99f85bbe9efd164e3e6e5297698c838e5313b
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 08:24:08 +0000

    Files extracted from 169273.tape

---
+++ curr11/lk3450.cmd
@@ -0,0 +1,8 @@
+SV3450.IMG/img/b:1000,MP3450.MAP=CG3450,RU3450,
+execmm,error,pkt1,pkt2,pkt3d,
+cmdtym,cmds,trm,trmf,trmp,
+trmt,zapbuf,status,
+lnk1,lnk2,lnkf,LNKF1,
+LNKIA,LKIBS1,LKIBS2,TYMA,TYMB,
+lnkc,lnkrsp,loddum,
+lina,ONCE,END
