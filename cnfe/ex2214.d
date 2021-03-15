3b5e830a1a3be60ee460a2d131f11f5dff9d4c1d
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:31:40 +0000

    Files extracted from 169273.tape

---
+++ cnfe/ex2214.d
@@ -0,0 +1,85 @@
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 2214                 DISTRICT: DALLAS  86/01/14  05:34
+LAST PROJECT NUMBER : 15656
+FIELD ENGINEER CONTACT : DAN MCLAUGHLIN
+
+********************************************************************
+                                 **LOCATION**
+CUSTOMER NAME : EXXON   ADDRESS : 1201 EAST AIRPORT FREEWAY
+CITY/STATE/ZIP :                            DALLAS TX. 75062
+CUSTOMER PHONE : 214/579-3805 TYMNET FE PHONE : 214/579-3840
+
+********************************************************************
+                                 **CONTACTS**
+8-5PM : MR. CARUTHERS      WORK : 214/579-3840
+
+AFTER 5 : NONE    TEL : NONE
+
+********************************************************************
+                                 **ACCESS COMMENTS**
+ENTRY CAN BE ATTAINED BETWEEN THE HOURS OF 08:00 AND 15:00.
+FOR ANY ACCESS CALL FIRST.
+**
+
+
+********************************************************************
+PVC OR PAP CODE REQUIRED? N   HIGH SPEED ASYNC? N
+********************************************************************
+                                 **HARDWARE**
+SERIAL NUMBER: 1399
+MACHINE TYPE : MINI   MACHINE CONFIG. TYPE: ISIS
+KERNEL HOST # = 214   FILENAME= (EXXNET:37)ET2214.BND
+REV LEVEL OF M/F = K2/M2 (430118-001) S.N.= B120
+REV LEVEL OF ROM I/O = H4/L3/L (430136-001) S.N.= 2480
+REV LEVEL OF CPU = K (430137-001) S.N.= 7007-0029
+REV LEVEL OF MAC = E1/D/S1 (430109-001) S.N.= B361
+MEMORY PART NUMBER AND REV LEVEL = 430147-001 / F1/D1 S.N.= B270
+SYNC BOARD  1 IS REV LEVEL H1 (430111-001) S.N.= B191
+ASYNC BOARD  1 IS REV LEVEL A1 (430110-001) S.N.= B299
+SIO MOTHER BOARD  1 IS REV LEVEL G1/L2 (430123-001) S.N.= B127
+THERE ARE  4 HOST SLOTS
+
+********************************************************************
+                                 **TELCO TROUBLE**
+TELCO TROUBLE NUMBER :214/824-1121
+
+********************************************************************
+                                 **NETWORK LINKS**
+LINE     0 = 9.6 H/W 2204/204/2
+SIO LINE 1 = V.24 H/W TO NODE 2204 KH=204 L=MREF(0/2)
+********************************************************************
+                                  **PSM INFO**
+ONE MAIN CARD CAGE (650015-001)
+THERE IS A CONTROL CARD (430208-001) REV= F S.N.= 1156-0076
+THIS 4 CHANNEL SYNC UNSWITCHED NON-SIO CARD (430193-001) IS REV= D S.N.= S10029-0092
+THIS 8 CHANNEL ASYNC UNSWITCHED NON-SIO CARD (430192-001) IS REV= D S.N.= S11017-0024
+THIS 8 CHANNEL ASYNC UNSWITCHED NON-SIO CARD (430192-001) IS REV= D S.N.= S11017-006
+THIS 8 CHANNEL ASYNC UNSWITCHED NON-SIO CARD (430192-001) IS REV= D S.N.= S11017-0017
+THIS 8 CHANNEL ASYNC UNSWITCHED NON-SIO CARD (430192-001) IS REV= D S.N.= S11017-0025
+THIS 4 CHANNEL SYNC RS232 SIO UNSWITCHED CARD (430212-001) IS REV= E S.N.= 0V07025-0019
+THIS 4 CHANNEL SYNC RS232 SIO UNSWITCHED CARD (430212-001) IS REV= E S.N.= 1071-0024
+THIS 8 CHANNEL ASYNC RS232 SIO UNSWITCHED CARD (430213-001) IS REV= B S.N.= 1049-0055
+********************************************************************
+                            CUSTOMER INTERFACE SLOTS
+
+SLOT  1   HOST#= NA   CUSTOMER= EXXON
+ADDRESS: SAME AS ABOVE   CITY/STATE/ZIP: SAME
+PHONE: SAME   CONTACT= SAME   HOST TYPE: SDLC
+LOGICAL UNITS=  1  AREFS=  0  MREFS=  1  SREFS=  0
+
+SLOT  2   HOST#= NA   CUSTOMER= EXXON
+ADDRESS: SAME   CITY/STATE/ZIP: SAME
+PHONE: SAME   CONTACT= SAME   HOST TYPE: TINET TIF
+LOGICAL UNITS=  1  AREFS=  1  MREFS=  0  SREFS=  0
+
+SLOT  3   HOST#= NA   CUSTOMER= EXXON
+ADDRESS: SAME   CITY/STATE/ZIP: SAME
+PHONE: SAME   CONTACT= SAME   HOST TYPE: X.25
+LOGICAL UNITS=  1  AREFS=  0  MREFS=  1  SREFS=  0
+
+SLOT  4   HOST#= 47   CUSTOMER= EXXON
+ADDRESS: SAME   CITY/STATE/ZIP: SAME
+PHONE: SAME   CONTACT= SAME   HOST TYPE: CONSAT
+LOGICAL UNITS=  1  AREFS=  1  MREFS=  0  SREFS=  0
+
+********************************************************************
