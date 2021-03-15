80ca021c9966f4294f0ed7737de4a87d3a613983
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 08:16:54 +0000

    Files extracted from 169273.tape

---
+++ curr11/lk112.cmd
@@ -0,0 +1,8 @@
+SV112.IMG/img/b:1000,MP112=CG112,RU112,
+execmm,error,pkt1,pkt2,pkt3d,
+cmdtym,cmds,trm,trmf,trmp,
+trmt,zapbuf,status,
+lnk1,lnk2,lnkf,LNKF1,
+LNKHA,LKHBS1,LKHBS2,LKHBS3,
+TYMA,TYMB,
+lina,ONCE,END
