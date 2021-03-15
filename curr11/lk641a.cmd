201b471c7cf77d249d08cf1f0a68596768b2f755
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:50:36 +0000

    Files extracted from 169273.tape

---
+++ curr11/lk641a.cmd
@@ -0,0 +1,10 @@
+SV641A.IMG/IMG/B:1000,MP641A.MAP=CG641A.OBJ,RU641A.OBJ,
+exec.OBJ,error.OBJ,pkt1.OBJ,pkt2.OBJ,pkt3d.OBJ,
+cmds.OBJ,cmdtym.OBJ,trm.OBJ,trmf.OBJ,trmp.OBJ,trmt.OBJ,
+ZAPBUF.OBJ,lina.OBJ,linb.obj,auto13.obj,TYMA.OBJ,tymb.OBJ,
+lnk1.OBJ,lnk2.OBJ,
+lnkf.OBJ,lnkf2.OBJ,
+lnkb.OBJ,lnkbs.OBJ,lnkflg.OBJ,crcbyt.OBJ,lkbduv.OBJ,
+status.OBJ,load.OBJ,
+once.OBJ,end.OBJ
+
