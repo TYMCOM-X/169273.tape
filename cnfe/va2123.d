3876a70494869caa6de70c503efaaf5707a0831c
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:31:44 +0000

    Files extracted from 169273.tape

---
+++ cnfe/va2123.d
@@ -0,0 +1,79 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE:VA2123  DISTRICT: DALLAS    DATE BLACKBOOK UPDATED:10/3/85
+LAST PROJECT NUMBER:13433&12105  DATE COMPLETED :09/26/85
+FIELD ENGINEER CONTACT :COLEMAN RHOADES  MIKE HAMILTON
+
+******************************* L O C A T I O N *******************************
+CUSTOMER NAME: V.A. MEDICAL CENTER    ADDRESS:4800 MEMORIAL DR. BLDG.4
+CITY/STATE/ZIP: W A C O ,  T E X A S      76703
+CUSTOMER PHONE:817/752-6581X695       TYMNET FE PHONE:SAME
+
+******************************* C O N T A C T S *******************************
+8-5PM M-F: DAVE SKINNER             WORK:752-6581
+                                    WORK:                   HOME:
+*AFTER 5:                           TEL:
+
+********************************* A C C E S S *********************************
+COMMENTS:   CALL SITE BEFORE DISPATCHING......
+              DRIVE AROUND TO THE BACK OF BLDG.4-- GO UP THE FIRE
+        ESCAPE,2ND FLOOR.....GO TO THE DATA PROCESSING ROOM.....ITS
+          LABELED ON THE DOOR.
+*******************************************************************************
+PVC OR PAP CODE REQUIRED?NO      HIGH SPEED ASYNC? YES
+*******************************************************************************
+
+ASSET TAG #
+******************************* H A R D W A R E *******************************
+SERIAL NUMBER: 1069
+MACHINE TYPE:MINI ENG.          ;MACHINE CONFIG. TYPE: ISIS
+KERNEL HOST # =1123             ;M/F BOARD = 3 POS.
+FILE NAME = (VACODE)2123.BND    ;FIRMWARE = SYNC ENHANCED
+MAC TYPE = MAC II               ;MEMORY TYPE & AMOUNT = ZITEL SEMI 1MEG.
+NO. OF SYNC CARDS =1            ;NO. OF ASYNC CARDS =1
+NO. OF SIO MBS =1               ;NO. OF SIO DBS =2
+TOTAL PARADYNES =1              ;TYPE= LSI 24
+NO. OF HOST SLOTS =2            ;NO. OF DATATROLS =O
+NO. OF VADICS =0                ;NO. OF 202T'S =0
+NO. OF CONNECTOR PANELS =1      ;NO. OF MODEM BREAKOUT PANELS =0
+FCO'S COMPLETE?                 ;NO. OF ASYNC BREAKOUT PANELS =
+NO. OF PRINTERS =
+
+************************ PAP AND/OR PVC INFORMATION ***************************
+                       N O N E
+
+
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO:                    LOCAL:
+
+************** N E T W O R K   N O D E   &   H O S T   L I N K S **************
+LINE OO = 9.6 FDEA291391  2505  KH.      L-
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+LINE XX =
+************** C U S T O M E R    I N T E R F A C E   S L O T S ***************
+SLOT 1   HOST#=     ;CUSTOMER=
+ADDRESS:                     ;CITY/STATE/ZIP:
+PHONE:               ; CONTACT=                ; HOST TYPE: 
+L.U. =     ;A.REF =       ;S.REF =      ;M.REF =
+SLOT 2   HOST#=     ;CUSTOMER=
+ADDRESS:                     ;CITY/STATE/ZIP:
+PHONE:               ; CONTACT=                ; HOST TYPE:
+L.U. =     ;A.REF =       ;S.REF =      ;M.REF =
+
+************************** PARADYNE SERIAL NUMBERS ****************************
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
+CKT #                  ;S/N         :CKT #                  ;S/N
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
