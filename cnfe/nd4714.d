1aba5028f321f6352ccccaea53aef73027063c1b
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:36:05 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd4714.d
@@ -0,0 +1,63 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 4714   DISTRICT: DALLAS    DATE BLACKBOOK UPDATED: 12 AUG. 1985
+LAST PROJECT NUMBER:             DATE COMPLETED :
+FIELD ENGINEER CONTACT :DAN MCLAUGHLIN
+
+******************************* L O C A T I O N *******************************
+CUSTOMER NAME: TYMSHARE/MARS PLUS     ADDRESS: 6333 HARRY HINES BLVD.
+CITY/STATE/ZIP: DALLAS,TX. 75235
+CUSTOMER PHONE:                       TYMNET FE PHONE:
+
+******************************* C O N T A C T S *******************************
+8-5PM M-F: ALEX MARTELL             WORK: 214/637-7435
+
+*AFTER 5: DCO OPERATORS             TEL: 214/637-7435
+
+********************************* A C C E S S *********************************
+COMMENTS: THE BLD'G IS MANNED 24 HRS A DAY; 7 DAYS A WEEK
+
+
+
+*******************************************************************************
+PVC OR PAP CODE REQUIRED? NO     HIGH SPEED ASYNC? NO
+*******************************************************************************
+
+******************************* H A R D W A R E *******************************
+MACHINE TYPE: TYMNET ENGINE     ;MACHINE CONFIG. TYPE: ISIS II CONSAT
+KERNEL HOST # = 5149            ;M/F BOARD = 3 POS. SWITCH
+FILE NAME = (TYMNET)ND4714.BND  ;FIRMWARE = 
+MAC TYPE = MAC II               ;MEMORY TYPE & AMOUNT = SEMI (1024)
+NO. OF SYNC CARDS = 1           ;NO. OF ASYNC CARDS = 0
+NO. OF SIO MBS = 0              ;NO. OF SIO DBS = 0
+TOTAL PARADYNES = 0             ;MPX = 0        ;MP96 = 0
+NO. OF HOST SLOTS = 2           ;NO. OF DATATROLS = 0
+NO. OF VADICS = 0               ;NO. OF 202T'S = 0
+NO. OF CONNECTOR PANELS = 1     ;NO. OF MODEM BREAKOUT PANELS = 0
+FCO'S COMPLETE? YES             ;NO. OF ASYNC BREAKOUT PANELS = 0
+NO. OF PRINTERS = 0
+     
+************************ PAP AND/OR PVC INFORMATION ***************************
+
+
+
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO: 214/823-4171       LOCAL: 214/824-1121
+
+************** N E T W O R K   N O D E   &   H O S T   L I N K S **************
+LINE 00 = (9.6) TO ND4070; KH# 3961; L# 8
+LINE 01 = (9.6) TO ND4713; KH# 5148; L# 11
+LINE 02 = (9.6) TO ND4067; KH# 3960; L# 15
+LINE 03 = OPEN
+************** C U S T O M E R    I N T E R F A C E   S L O T S ********
+SLOT 1   HOST#=     ;CUSTOMER=
+ADDRESS:                     ;CITY/STATE/ZIP:
+PHONE:               ; CONTACT=                ; HOST TYPE:
+L.U. =     ;A.REF = N/A   ;S.REF =        ;M.REF = N/A
+SLOT 2   HOST#=     ;CUSTOMER=
+ADDRESS:                     ;CITY/STATE/ZIP:
+PHONE:               ; CONTACT=                ; HOST TYPE:
+L.U. =     ;A.REF = N/A   ;S.REF =            ;M.REF = N/A
+
+************************** PARADYNE SERIAL NUMBERS ****************************
+CKT #                  ;S/N         :CKT #                  ;S/N
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
