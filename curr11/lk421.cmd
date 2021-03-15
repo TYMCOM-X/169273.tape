b39594b5370fcb224afd11bb660371ae6b1d9e99
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 08:06:57 +0000

    Files extracted from 169273.tape

---
+++ curr11/lk421.cmd
@@ -0,0 +1,7 @@
+SV421.IMG/img/b:1000,MP421.MAP=CG421.OBJ,RU421.OBJ,
+exec.OBJ,error.OBJ,pkt1.OBJ,pkt2.OBJ,pkt3d.OBJ,
+cmdtym.OBJ,cmds.OBJ,trm.OBJ,trmf.OBJ,trmp.OBJ,
+trmt.OBJ,zapbuf.OBJ,status.OBJ,
+lnk1.OBJ,lnk2.OBJ,lnkf.OBJ,LNKF1.OBJ,
+LNKHA,LKHBS1,LKHBS2,LKHBS3,TYMA,TYMB,
+lina.OBJ,ONCE,END
