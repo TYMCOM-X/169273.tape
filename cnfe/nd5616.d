26f45aee5d4f94f448c6f6fab03aa2e10707cbb5
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:37:55 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd5616.d
@@ -0,0 +1,49 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 5616   DISTRICT: DALLAS    DATE BLACKBOOK UPDATED: NOV 06,1986
+LAST PROJECT NUMBER: 18427CDA001 DATE COMPLETED : NOV 02, 1986
+FIELD ENGINEER CONTACT : BILL SCHNELLER
+******************************* L O C A T I O N *******************************
+CUSTOMER NAME: TYMNET INC.            ADDRESS: 6333 HARRY HINES BLVD.
+CITY/STATE/ZIP: DALLAS/TEXAS/75235
+CUSTOMER PHONE: 214/637-7316          TYMNET FE PHONE:
+******************************* C O N T A C T S *******************************
+8-5PM M-F: 214/637-7316             WORK:                   HOME:
+AFTER 5: STANDBY PERSON             TEL:
+********************************* A C C E S S *********************************
+COMMENTS:
+*******************************************************************************
+PVC OR PAP CODE REQUIRED?NO      HIGH SPEED ASYNC?NO
+******************************* H A R D W A R E *******************************
+SERIAL NUMBER: 2308             ASSET TAG NUMBER: 249147
+MACHINE TYPE: FULL ENGINE       ;MACHINE CONFIG. TYPE: ISIS II
+KERNEL HOST # = 7161            ;M/F BOARD = LBOOT 6
+FILE NAME =(TYM5)ND5616.BND       ;FIRMWARE = VERSION 18
+MAC TYPE = MAC II               ;MEMORY TYPE & AMOUNT = SEMI 1 MEG
+NO. OF SYNC CARDS = 1           ;NO. OF ASYNC CARDS = 0
+NO. OF SIO MBS = 1              ;NO. OF SIO DBS = 2 V.24, 1 OCL
+TOTAL PARADYNES =               ;MPX =          ;MP96 =
+NO. OF HOST SLOTS = 4           ;NO. OF DATATROLS =
+NO. OF VADICS =                 ;NO. OF 202T'S =
+NO. OF CONNECTOR PANELS = 1     ;NO. OF MODEM BREAKOUT PANELS = 1
+FCO'S COMPLETE?                 ;NO. OF ASYNC BREAKOUT PANELS =
+NO. OF PRINTERS =
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO: 214/823-4171       LOCAL: 214/824-1121
+************** N E T W O R K   N O D E   &   H O S T   L I N K S **************
+LINE 00 = UNUSED (XLINK)
+LINE 01 = MS ND5610 KH#7137 LINE 15
+LINE 02 = MS ND5377 KH#6846 LINE 17
+************** C U S T O M E R   I N T E R F A C E   S L O T S ****************
+SLOT 1   HOST#= UNUSED
+SLOT 2   HOST#=7132 ;CUSTOMER= MDC NSC
+HOST TYPE= NETWORK SERVER(CODE ONLY)
+SLOT 3  HOST#=6758 ;CUSTOMER= HOME SAVINGS  HOST TYPE= X.25 HDLC
+M.REF (O,O) CUSTOMER CONTACT DEBBIE CARMICHAEL AT 386-8088 (8-5)
+SLOT 4 HOST#=8583 ;CUSTOMER= MDC-IPC/DYNATAX     HOST TYPE=X.25H
+M.REF (0,1) CUSTOMER CONTACT :
+SLOT 5 HOST#=2871 ;CUSTOMER= MTECH               HOST TYPE=X.25HDLC
+M.REF (0,2) CUSTOMER CONTACT : ALAN PARKER 214/744-5638 
+************************** PARADYNE SERIAL NUMBERS ****************************
+CKT # 11FDPA028727     ;S/N 200559  ;A/N 249248             ;CAB 3,73
+CKT # 214/951-0581     ;S/N 2050    ;A/N NONE        A.J. MODEM
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
