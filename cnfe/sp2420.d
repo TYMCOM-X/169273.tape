c74038071f51b90a2ad01c2ea18de8a2221eeb77
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:38:33 +0000

    Files extracted from 169273.tape

---
+++ cnfe/sp2420.d
@@ -0,0 +1,57 @@
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE: 2420                 DISTRICT: DALLAS  86/02/27  12:12
+LAST PROJECT NUMBER : 
+FIELD ENGINEER CONTACT : CLEMAN RHOADES - RON BARDIN
+
+********************************************************************
+                                 **LOCATION**
+CUSTOMER NAME : SWB  ADDRESS : 301 WHALEY 5TH FLOOR
+CITY/STATE/ZIP :                            LONGVIEW
+CUSTOMER PHONE : 214-236-1308 TYMNET FE PHONE : -
+
+********************************************************************
+                                 **CONTACTS**
+8-5PM : LEO BRIAN    WORK : 214-236-1308
+
+AFTER 5 : -    TEL : -
+
+********************************************************************
+                                 **ACCESS COMMENTS**
+GUARD AT ENTRANCE AND CALL CUSTOMER CONTACT BEFORE DISPATCHING
+**
+
+
+
+********************************************************************
+PVC OR PAP CODE REQUIRED? NO  HIGH SPEED ASYNC? NO
+********************************************************************
+                                 **HARDWARE**
+SERIAL NUMBER: 940 810023
+MACHINE TYPE : MINI   MACHINE CONFIG. TYPE: ISIS
+KERNEL HOST # = 932   FILENAME= -
+ASSET TAG NUMBER = -
+REV LEVEL OF M/F = H (430118-001) S.N.= 2605-0035
+REV LEVEL OF ROM I/O = L (430136-001) S.N.= OV9004 00034
+REV LEVEL OF CPU = K (430137-001) S.N.= 0042
+REV LEVEL OF MAC = E (430109-001) S.N.= 80240003
+MEMORY PART NUMBER AND REV LEVEL = F S.N.= 8022-0091
+SYNC BOARD  1 IS REV LEVEL H (430111-001) S.N.= OV2514-0019
+SYNC BOARD  2 IS REV LEVEL H (430111-001) S.N.= 8002-0036
+ASYNC BOARD  1 IS REV LEVEL I/O II (430110-001) S.N.= 935-0002
+CONNECTOR PANEL  1 IS PART # AND REV LEVEL : CBB219 REV A
+THIS CONNECTOR PANEL IS S.N. = F9228 5010
+CONNECTOR PANEL  2 IS PART # AND REV LEVEL : CBB219 REV A
+THIS CONNECTOR PANEL IS S.N. = F1125 0021
+CONNECTOR PANEL  3 IS PART # AND REV LEVEL : CBB219 REV A
+THIS CONNECTOR PANEL IS S.N. = F1125 0024
+
+********************************************************************
+                                 **TELCO TROUBLE**
+TELCO TROUBLE NUMBER :0
+
+********************************************************************
+                                 **NETWORK LINKS**
+LINE     0 = 2421-933-00-9.6
+LINE     1 = 2374-886-01-9.6
+LINE     2 = 2501-1013-01-9.6
+********************************************************************
