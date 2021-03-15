a82f67110dd6643eb65de8aa306293d2bcc5c567
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 08:04:43 +0000

    Files extracted from 169273.tape

---
+++ curr11/lk5274.cmd
@@ -0,0 +1,9 @@
+SV5274.IMG/img/b:1000,MP5274.MAP=CG5274,RU5274,
+exec,error,pkt1,pkt2,pkt3d,
+cmdtym,cmds,trm,trmf,trmp,
+trmt,zapbuf,status,auto13,
+lnk1,lnk2,lnkf,lnkf2,
+lnkia,lkibs1,lkibs2,tyma,tymb,
+lnkb,lnkbs,lkbduv,lnkflg,crcbyt,load,
+lina,linb,once,end
+
