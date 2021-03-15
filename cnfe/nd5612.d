7e36c99a779a48a141a105821cc2234cb84b6f9f
Lars Brinkhoff <lars@nocrew.org>
2021-03-15 07:32:39 +0000

    Files extracted from 169273.tape

---
+++ cnfe/nd5612.d
@@ -0,0 +1,61 @@
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NODE:5612    DISTRICT: DALLAS    DATE BLACKBOOK UPDATED:MAY 23,1986
+LAST PROJECT NUMBER:15623CD      DATE COMPLETED : 05-23-86
+FIELD ENGINEER CONTACT : ED DOBBS
+
+******************************* L O C A T I O N *******************************
+TYMNET SITE: TYMNET INC.              ADDRESS: 6333 HARRY HINES BLVD.
+CITY/STATE/ZIP: DALLAS,TEXAS 75235
+
+
+******************************* H A R D W A R E *******************************
+SERIAL NUMBER: 2304
+MACHINE TYPE: FULL BLOWN ENGINE ;MACHINE CONFIG. TYPE: SWITCHER
+KERNEL HOST # =7139             ;M/F BOARD = 3 POSITION SWITCH
+FILE NAME =(TYM4000)ND5612      ;FIRMWARE =
+MAC TYPE = MAC II               ;MEMORY TYPE & AMOUNT = 1024 SEMI
+NO. OF SYNC CARDS = 1           ;NO. OF ASYNC CARDS = 0
+NO. OF SIO MBS = 1              ;NO. OF SIO DBS = 3
+TOTAL PARADYNES = 0             ;MPX = 0        ;MP96 = 0
+NO. OF HOST SLOTS = 0           ;NO, OF DATATROLS = 0
+NO. OF VADICS = 0               ;NO. OF 202T'S= 0
+NO. OF CONNECTOR PANELS = 1     ;NO. OF MODEM BREAKOUT PANELS = 0
+FCO'S COMPLETE? YES             ;NO. OF ASYNC BREAKOUT PANELS = 0
+NO. OF PRINTERS = 0
+
+
+************************ TELCO TROUBLE NUMBERS ********************************
+TELCO: 214/823-4171       LOCAL:214/824-1121
+
+********************** N E T W O R K   L I N K S ******************************
+LINE 00 = 9.6    ND2700   KH#6759  (L-01)
+LINE 01 = 9.6    ND2722   KH#6760  (L-01)
+LINE 02 = 9.6    ND5621   KH#7165  (L-01)
+LINE 03 = 9.6    ND5620   KH#7163  (L-01)
+LINE 04 = OPEN
+LINE 05 = 9.6    ND5617   KH#7162  (L-01)
+LINE 06 = 9.6    ND5616   KH#7161  (L-01)
+LINE 07 = 9.6    ND5615   KH#7160  (L-01)
+LINE 08 = 9.6    ND7067   KH#4390  (L-02)
+LINE 09 = 9.6    ND7071   KH#5147  (L-02)
+LINE 10 = 9.6    ND7070   KH#4670  (L-03)
+LINE 11 = 9.6    ND7072   KH#5221  (L-02)
+LINE 12 = 9.6    ND7530   KH#3800  (L-03)
+LINE 13 = 9.6    ND7531   KH#3801  (L-03)
+LINE 14 = 9.6    ND7532   KH#3802  (L-03)
+LINE 15 = 9.6    N10344   KH#1182  (L-03)
+LINE 16 = MS SIO  ND2347   KH#2125  (L-23)
+LINE 17 = MS SIO  ND2211   KH#1465  (L-17)
+LINE 18 = MS SIO  ND5331   KH#6731  (L-17)
+LINE 19 = MS SIO  ND5611   KH#7138  (L-17)
+LINE 20 = OPEN
+LINE 21 = MS SIO  ND5401   KH#6848  (L-25)
+************************** PARADYNE SERIAL NUMBERS ****************************
+CKT #                  ;S/N         :CKT #                 ;S/N
+CKT #                  ;S/N         :CKT #                 ;S/N
+CKT #                  ;S/N         :CKT #                 ;S/N
+CKT #                  ;S/N         :CKT #                 ;S/N
+CKT #                  ;S/N         :CKT #                 ;S/N
+CKT #                  ;S/N         :CKT #                 ;S/N
+CKT #                  ;S/N         :CKT #                 ;S/N
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
