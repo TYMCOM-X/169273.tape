21258eb4136724f9a48ea91d740dc2d17515d741
Lars Brinkhoff <lars@nocrew.org>
2021-03-17 12:24:17 +0000

    Files extracted from 169273.tape

---
+++ new11/lk2377.cmd
@@ -0,0 +1,9 @@
+sv2377/img/b:1000:lowmem:sx:pure:main/b:10000:falcbt/b:11000
+/T:167400:pdata:ddb:ldb:adb:sdb:impure,
+MP2377=cg2377.nbj,ru2377.nbj,
+exec.nbj,error.nbj,pkt1.nbj,pkt2.nbj,pkt3d.nbj,
+cmdsat.nbj,cmdsst.nbj,trmst.nbj,trmfst.nbj,trmpst.nbj,
+trmtst.nbj,zapbuf.nbj,
+lnk1.nbj,lkiadp.nbj,lkist1.nbj,lkist2.nbj,
+tyma.nbj,linast.nbj,status.nbj,
+oncrom.nbj,end.nbj
