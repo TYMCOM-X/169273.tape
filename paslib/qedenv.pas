2b43af74ad4f7ff326a0023325a7a783f7d75e31
Lars Brinkhoff <lars@nocrew.org>
2021-03-17 15:45:30 +0000

    Files extracted from 169273.tape

---
+++ paslib/qedenv.pas
@@ -0,0 +1,42 @@
+$TITLE EDENV.PAS, last modified 2/16/84, zw
+ENVMODULE qedenv;
+(*pascal editor environment*)
+
+$SYSTEM cmdutl
+$SYSTEM qerr.typ
+$SYSTEM qstr.typ
+$SYSTEM qspat.typ
+$SYSTEM qspred.typ
+$SYSTEM cmdutl.typ
+$SYSTEM qedln.typ
+$SYSTEM qline.typ
+$SYSTEM qld.typ
+$SYSTEM ed.typ
+$SYSTEM qsplit.typ
+$SYSTEM qsubst.typ
+$SYSTEM wio.typ
+$SYSTEM qedtyp.typ
+$SYSTEM filutl
+$SYSTEM qquery
+$SYSTEM qspat
+$SYSTEM qspred
+$SYSTEM qederr
+$SYSTEM qld
+$SYSTEM qread
+$SYSTEM qedln
+$SYSTEM qmark
+$SYSTEM qprint
+$SYSTEM qsubst
+$SYSTEM qjoin
+$SYSTEM qsplit
+$SYSTEM qopen
+$SYSTEM ed
+$IF P10
+$SYSTEM infpac
+$SYSTEM wio
+$END
+$IF VAX
+$SYSTEM imgnam
+$END
+$SYSTEM qlabel
+END.
