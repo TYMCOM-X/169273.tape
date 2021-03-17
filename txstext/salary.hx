481c02d01419c5946263749d5c9609ce0d348755
Lars Brinkhoff <lars@nocrew.org>
2021-03-17 21:45:22 +0000

    Files extracted from 169273.tape

---
+++ txstext/salary.hx
@@ -0,0 +1,17 @@
+
+				SALARY HISTORY
+
+Kaiser Foundation Hospital
+$27,000
+
+Blue Cross of California
+$28,600
+
+Shared Medical Systems
+$25,000
+
+Roth Young Personnel Service
+draw vs. commision
+
+Brattleboro Memorial Hospital
+$15,000
