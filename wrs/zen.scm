794dc3e29a62d2fe7898af901cafca3bb2709724
Lars Brinkhoff <lars@nocrew.org>
2021-03-17 23:17:35 +0000

    Files extracted from 169273.tape

---
+++ wrs/zen.scm
@@ -0,0 +1,24 @@
+I was going through some old papers and came across a copy of Edsger
+W. Dijkstra, "How do we tell truths that might hurt?", published in
+June 1975.  Here are a couple choice excerpts which I'm sure you will
+appreciate given your interest in languages.  Despite the first
+impression, the commentary is quite serious.
+
+-Bill
+
+----------------
+It is practically impossible to teach good programming to students
+that have had a prior exposure to BASIC: as potential programmers
+they are mentally mutilated beyond hope of regeneration.
+
+The use of COBOL cripples the mind; its teaching should, therefore,
+be regarded as a criminal offence.
+
+About the use of language: it is impossible to sharpen a pencil with a
+blunt axe.  It is equally vain to try to do it with ten blunt axes instead.
+
+Besides a mathematical inclination, an exceptionally good mastery of
+one's native tongue is the most vital asset of a competent programmer.
+
+The tools we use have a profound (and devious!) influence on our
+thinking habits, and, therefore , on our thinking abilities.
