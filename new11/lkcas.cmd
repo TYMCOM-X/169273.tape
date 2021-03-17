5f53d7d8fe322ebb829339b88eaa00ea37429e93
Lars Brinkhoff <lars@nocrew.org>
2021-03-17 12:26:41 +0000

    Files extracted from 169273.tape

---
+++ new11/lkcas.cmd
@@ -0,0 +1,11 @@
+svCAS/img
+/b:1000:lowmem:sx:inidat:inibyt:main:second:tenths:init:.xdt.:once:vectbl
+/b:10000:falcbt/b:10040
+/T:167400:pdata:ddb:ldb:adb:sdb:impure,
+MPCAS=cgCAS.nbj,ruCAS.nbj,
+exec.nbj,errrom.nbj,pkt1.nbj,pkt2.nbj,pkt3d.nbj,
+cmdsat.nbj,cmdsst.nbj,trmsC.nbj,trmfst.nbj,trmpst.nbj,
+trmtsC.nbj,xltcrs.nbj,xltebc.nbj,zapbuf.nbj,
+lnk1.nbj,lnkiSH.nbj,lkist1.nbj,lkist2.nbj,
+tyma.nbj,lingst.nbj,ATCMGR.nbj,
+aCAS11.nbj,oncrom.nbj,end.nbj
