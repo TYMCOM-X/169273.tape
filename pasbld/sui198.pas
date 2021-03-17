d56e5070ae96e2464f87e81960c3a7038a2aaa5c
Lars Brinkhoff <lars@nocrew.org>
2021-03-17 14:32:12 +0000

    Files extracted from 169273.tape

---
+++ pasbld/sui198.pas
@@ -0,0 +1,33 @@
+
+(****           (c) 1981 Strategic Information                  ****)
+(****           division of Ziff Davis Publishing Co.           ****)
+
+(* program number 198*)
+(*TEST 6.6.6.3-2, CLASS=ERRORHANDLING*)
+(* This program causes an error to occur as the result
+  returned by the trunc function is not a value of the
+  type integer.
+  The error should be detected at run-time. *)
+program t6p6p6p3d2;
+var
+   reel : real;
+   i    : integer;
+   ok   : boolean;
+begin
+        rewrite(output,'suite.txt',[preserve]);       writeln('suite program #198');
+   reel:=11111.11111;
+   ok:=true;
+   while ok do
+   begin
+      i:=trunc(reel);
+      if (i<0) then
+         ok:=false
+
+(****           (c) 1981 Strategic Information                  ****)
+(****           division of Ziff Davis Publishing Co.           ****)
+
+      else
+         reel:=reel*2
+   end;
+   writeln(' ERROR NOT DETECTED...6.6.6.3-2')
+end.
