c65fb553cc3d19c5b65481604a1901d40f395e5f
Lars Brinkhoff <lars@nocrew.org>
2021-03-17 23:00:44 +0000

    Files extracted from 169273.tape

---
+++ wrs/army.pas
@@ -0,0 +1,41 @@
+--none--           BRS          [30160,264452] h5678 ic np
+--none--           BUBB         [32434,337121] h6102 tp nb np
+--none--           BUY2         [44756,75745]  h7456 ic np
+--none--           CCA          [26033,263260] h7275 ic np
+--none--           CIC          [37606,227025] h7210 ic np
+--none--           CIS02        [34521,350754] h6162 ic np
+--none--           CPS          [22637,205507] h6427 ic np
+--none--           CPS01        [34521,350753] h6162 ic np
+--none--           DELPHI       [43352,234117] h7322 ic np
+--none--           DIA52        [33233,54156]  h6649 ic np
+--none--           DIALCOM      [33233,323272] h6907 ic np
+--none--           DIALOG       [16475,271654] h5734 ic np
+--none--           DIALOG2      [32361,26242]  h6505 ic np
+--none--           EDOM         [33546,24722]  h7519 ic np
+--none--           EMSACORPS    [33060,254072] h7265 ic np
+--none--           EMSAW        [33060,131124] h7427 ic np
+--none--           EMSCOMP      [33060,113561] h7427 ic np
+--none--           EMSDIX       [33060,5102]   h6386 ic np
+--none--           EMSTYM       [33060,366547] h6274 ic np
+--none--           ENERTECH     [27732,34057]  h6544 ic np
+--none--           INFORMATION  [10056,136455] h7313 nb ic np
+--none--           NETEVOP      [27732,364313] h6258 np
+--none--           OAG          [36645,53072]  h6641 ic np
+--none--           PH           [43652,230141] h7216 np
+--none--           SAI          [41742,143552] h7527 ic np
+--none--           TIMELIFE     [26137,171067] h7030 ic np
+--none--           UNINETVIARCA [26137,155504] h6993 ic np
+--none--           VAX2X        [32376,254054] h7133 ic np
+--null--           1WUI409TS    [22374,367127] h6280 ic
+--null--           2107RCAP     [26137,106753] h6805 ic
+--null--           31132341RCA  [26137,111010] h6818 ic
+--null--           31132342RCA  [26137,111023] h6818 ic
+--null--           C2           [21650,203060] h6426 ic
+--null--           DOW1         [21351,177046] h6426 ic
+--null--           XCSGTS       [26505,246560] h5489 ic
+CUC                CUC          [15720,146724] h4297 ic oh [old]
+JETPLAN            LAS          [17350,156104] h7092 ic
+NEW USER           ATHENS       [44337,256760] h7352 ic
+PASS WORD          M33          [3,42754]      h7147 tp ty nb
+XXX                UNCL         [23155,211172] h6427 ic
+YHN7UJM            NCUD1        [6,223214]     h7165 tp as ty nb
