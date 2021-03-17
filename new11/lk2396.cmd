05ea6399dc710ce0b883ac9d0747ada32bc26c9f
Lars Brinkhoff <lars@nocrew.org>
2021-03-17 12:26:35 +0000

    Files extracted from 169273.tape

---
+++ new11/lk2396.cmd
@@ -0,0 +1,12 @@
+sv2396/img
+/b:1000:lowmem:sx:inidat:inibyt:main:second:tenths:init:.xdt.:once:vectbl
+/b:10000:falcbt/b:10040
+/T:167400:pdata:ddb:ldb:adb:sdb:impure,
+MP2396=cg2396.nbj,ru2396.nbj,
+exec.nbj,errrom.nbj,pkt1.nbj,pkt2.nbj,pkt3d.nbj,CMDTYM.NBJ,
+cmdx28.nbj,cmdt28.nbj,cmds.nbj,trm.nbj,trmf.nbj,trmptp.nbj,
+trmt.nbj,xltcrs.nbj,xltebc.nbj,zapbuf.nbj,
+lnk1.nbj,lnk2.nbj,lnkeb.nbj,lkepkt.nbj,x25q.nbj,
+nolnki.nbj,
+tyma.nbj,ling.nbj,padmgr.nbj,
+auto11.nbj,oncrom.nbj,end.nbj
