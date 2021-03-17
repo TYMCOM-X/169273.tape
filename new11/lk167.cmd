1a4ca66d7a899b984d0404da6e01262f07e7bb21
Lars Brinkhoff <lars@nocrew.org>
2021-03-17 12:24:07 +0000

    Files extracted from 169273.tape

---
+++ new11/lk167.cmd
@@ -0,0 +1,8 @@
+SV167.IMG/img/b:1000,MP167=CG167,RU167,
+exec,error,pkt1,pkt2,pkt3d,
+cmdtym,cmds,trm,trmf,trmp,
+trmt,zapbuf,status,
+lnk1,lnk2,lnkf,lnkf1,
+lnkrsp,lnkc,load,
+lnkha,lkhbs1,lkhbs2,lkhbs3,tyma,tymb,
+lina,once,end
