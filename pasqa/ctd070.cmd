590365a4453b9f11159b1e3f4b0ab5ec650661a1
Lars Brinkhoff <lars@nocrew.org>
2021-03-17 16:26:45 +0000

    Files extracted from 169273.tape

---
+++ pasqa/ctd070.cmd
@@ -0,0 +1,12 @@
+pt007e
+:env pt007e
+/debug,mainseg,nooverlay
+ptd070=pt0070
+/debug,nomainseg,overlay
+ptd07a=pt007a
+ptd07b=pt007b
+ptd07c=pt007c
+ptd07d=pt007d
+ptd07i=pt007i
+ptd07j=pt007j
+/exit
