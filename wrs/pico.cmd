f1868ba1e937604b41c0d4589bb3e1e32a2e5c05
Lars Brinkhoff <lars@nocrew.org>
2021-03-17 23:28:36 +0000

    Files extracted from 169273.tape

---
+++ wrs/pico.cmd
@@ -0,0 +1,28 @@
+;;...................................................................
+;;.        T E L E R A T E  WORLD WIDE  S T A N D A R D             .
+;;.                                                                 .
+;;.                         << ISIS >>                              .
+;;.     Node: 2100                              Created: 01-Aug-88  .
+;;.   Kernel: 100                                    By: WTT        .
+;;.  Version: 10.01  				   SLOT: FF         .
+;;...................................................................
+;;.                    >>> Revision History <<<                     .
+;;.                    ------------------------			    .
+;;. 01-Aug-88 WTT Code generation.                                  .
+;;...................................................................
+;;
+;@(BETATEST)I2IS10.I01
+;@(TELCODE)PICO.TYM
+;@(TELCODE)XGO703.LST
+;@(BETATEST)I2IS10.R01
+;; Assemble patches if any
+;A(NTS)I2IS10.XXX
+1;F(TELCODE)PICO.NIB
+70;T0,SUMTBL;PINTLEN,IEND-INTLEN;P%PSEGF,SFSIZE;P
+;;
+;; End of Kernel Command File
+;;
+%Q
+
+
+
