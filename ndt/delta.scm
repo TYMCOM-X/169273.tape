3abee8dd56ab4ac757de3d0b2adf62202527c2b0
Lars Brinkhoff <lars@nocrew.org>
2021-03-17 12:03:16 +0000

    Files extracted from 169273.tape

---
+++ ndt/delta.scm
@@ -0,0 +1,13 @@
+File 1)	DSK:USEBAD.SAI[3,352477]	created: 1625 14-FEB-83
+File 2)	DSK:USEOK.SAI[3,352477]	created: 1626 14-FEB-83
+
+1)1	    Hold_ new!record( user );
+1)	    user:Usr[ Hold ]_ Name;
+****
+2)1	    user:Usr[ Hold ]_ Name;
+**************
+1)1	    AddUser( DulList, Name, PPN );
+****
+2)1	    Hold_ new!record( user );
+2)	    AddUser( DulList, Name, PPN );
+**************
